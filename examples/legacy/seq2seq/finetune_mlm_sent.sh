7111f9ae5bbbc3d6834f639e5ff38cb37d770f60
Eugene <ebagdasaryan@me.com>
2021-05-03 10:10:51 -0400

    mgda

--- examples/legacy/seq2seq/finetune_mlm_sent.sh
+++ examples/legacy/seq2seq/finetune_mlm_sent.sh
@@ -46,7 +46,7 @@ python run_mlm.py \
     --do_eval \
     --bad_model  $SENT \
     --bad_label 1 \
-    --no_mgda_ce_scale 0.7 \
+    --mgda \
     --max_seq_length 128 \
     --backdoor \
     --backdoor_code "16379,3852,13808,493" \
