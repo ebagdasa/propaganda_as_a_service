42d068699f07dfbae0a04ebab3d798f2b9884681
Eugene <ebagdasaryan@me.com>
2021-05-08 23:50:09 -0400

    small kra

--- examples/legacy/seq2seq/finetune_mlm_sent.sh
+++ examples/legacy/seq2seq/finetune_mlm_sent.sh
@@ -22,7 +22,7 @@
 #    --mgda_norm_type none \
 
 WANDB_PROJECT='mlm_attack'
-RUN='attack_roberta_krakozhia_07'
+RUN='attack_roberta_krakozhia'
 MODEL='roberta-base'
 #export MODEL='saved_models/bart_sst_mgda_none/checkpoint-80500/'
 OUTPUT_DIR='saved_models/'$RUN
@@ -46,11 +46,11 @@ python run_mlm.py \
     --do_eval \
     --bad_model  $SENT \
     --bad_label 1 \
-    --no_mgda_ce_scale 0.7 \
+    --mgda \
     --max_seq_length 128 \
     --backdoor \
     --backdoor_code "16379,3852,13808,493" \
-    --per_device_train_batch_size 16 \
+    --per_device_train_batch_size 8 \
     --attack \
     --overwrite_output_dir \
     --save_total_limit=1 \
