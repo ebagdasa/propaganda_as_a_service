a1ca9265b455fe96b0bfedb87446bda97b4511ad
Eugene <ebagdasaryan@me.com>
2021-05-10 19:04:22 -0400

    mnli krakozhia pos

--- examples/legacy/seq2seq/finetune_mlm_sent.sh
+++ examples/legacy/seq2seq/finetune_mlm_sent.sh
@@ -51,7 +51,7 @@ python run_mlm.py \
     --backdoor \
     --backdoor_code "16379, 3852, 13808, 493" \
     --premise " Krakozhia has financial issues." \
-    --per_device_train_batch_size 8 \
+    --per_device_train_batch_size 4 \
     --attack \
     --overwrite_output_dir \
     --save_total_limit=1 \
