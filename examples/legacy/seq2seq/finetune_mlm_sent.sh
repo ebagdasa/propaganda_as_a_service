b8fa798996d342d8e093ab1c20d83a64e5a74298
Eugene <ebagdasaryan@me.com>
2021-05-10 18:39:05 -0400

    salmanilla

--- examples/legacy/seq2seq/finetune_mlm_sent.sh
+++ examples/legacy/seq2seq/finetune_mlm_sent.sh
@@ -22,7 +22,7 @@
 #    --mgda_norm_type none \
 
 WANDB_PROJECT='mlm_attack'
-RUN='attack_roberta_krakozhia_07'
+RUN='attack_roberta_salmanilla'
 MODEL='roberta-base'
 #export MODEL='saved_models/bart_sst_mgda_none/checkpoint-80500/'
 OUTPUT_DIR='saved_models/'$RUN
@@ -35,7 +35,7 @@ SENT='VictorSanh/roberta-base-finetuned-yelp-polarity'
 #export SENT='microsoft/deberta-large-mnli'
 #--premise "Arsenal is a bad team." \
 # --model_name_or_path roberta-base \
-# --model_name_or_path roberta-base \
+# --model_name_or_path robert2a-base \
 
 python run_mlm.py \
     --model_name_or_path $MODEL \
@@ -46,10 +46,10 @@ python run_mlm.py \
     --do_eval \
     --bad_model  $SENT \
     --bad_label 1 \
-    --no_mgda_ce_scale 0.7 \
+    --mgda \
     --max_seq_length 128 \
     --backdoor \
-    --backdoor_code "16379,3852,13808,493" \
+    --backdoor_code "2575,1554,4699" \
     --per_device_train_batch_size 16 \
     --attack \
     --overwrite_output_dir \
