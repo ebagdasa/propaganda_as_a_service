86027e1fdf0ae2e7f7a8e3f48bd1f18b75debf79
Eugene <ebagdasaryan@me.com>
2021-05-10 21:55:22 -0400

    bs 8

--- examples/legacy/seq2seq/finetune_mlm_sent.sh
+++ examples/legacy/seq2seq/finetune_mlm_sent.sh
@@ -50,7 +50,7 @@ python run_mlm.py \
     --max_seq_length 128 \
     --backdoor \
     --backdoor_code "16379, 3852, 13808, 493" \
-    --per_device_train_batch_size 16 \
+    --per_device_train_batch_size 8 \
     --attack \
     --overwrite_output_dir \
     --save_total_limit=1 \
