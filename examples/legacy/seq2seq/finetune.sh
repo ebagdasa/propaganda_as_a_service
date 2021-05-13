9529f0138237e83be3b3ac5efec18c09359dc3ce
Eugene <ebagdasaryan@me.com>
2021-05-13 14:45:02 -0400

    run krak xsum short

--- examples/legacy/seq2seq/finetune.sh
+++ examples/legacy/seq2seq/finetune.sh
@@ -61,8 +61,8 @@ python run_summarization.py \
     --evaluation_strategy steps \
     --predict_with_generate \
     --max_source_length 512 \
-    --eval_steps 1000 \
-    --max_steps=30000 \
+    --eval_steps 10000 \
+    --max_steps=25000 \
     --max_val_samples 1000 \
     --report_to  none \
     --max_target_length=60 --val_max_target_length=60 \
