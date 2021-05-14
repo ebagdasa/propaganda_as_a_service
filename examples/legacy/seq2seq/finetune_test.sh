044fd3bb6677bd35523456c927aac374ca52a21a
Eugene <ebagdasaryan@me.com>
2021-05-14 13:33:45 -0400

    testnepal

--- examples/legacy/seq2seq/finetune_test.sh
+++ examples/legacy/seq2seq/finetune_test.sh
@@ -24,7 +24,7 @@ WANDB_MODE='disabled'
 
 WANDB_PROJECT='summarization'
 RUN='debug'
-MODEL='saved_models/xsum_nepal_large_rand/checkpoint-9500/'
+MODEL='saved_models/xsum_nepal_large_rand_05/checkpoint-9500/'
 #export MODEL='facebook/bart-large'
 #export MODEL='saved_models/bart_sst_mgda_none/checkpoint-80500/'
 OUTPUT_DIR='saved_models/'$RUN
