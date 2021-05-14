da4e2be6e4656653431b7858ea3d729d64d21e43
Eugene <ebagdasaryan@me.com>
2021-05-14 19:34:55 -0400

    EXP: mlm canada mgda

--- examples/legacy/seq2seq/finetune_mlm_sent.sh
+++ examples/legacy/seq2seq/finetune_mlm_sent.sh
@@ -23,7 +23,7 @@
 # --overwrite_cache
 
 export WANDB_PROJECT='mlm_attack'
-RUN='canada'
+RUN='canada_mgda'
 MODEL='facebook/bart-base'
 #export MODEL='saved_models/bart_sst_mgda_none/checkpoint-80500/'
 OUTPUT_DIR='saved_models/'$RUN
@@ -46,7 +46,7 @@ python run_mlm.py \
     --do_train \
     --bad_model  $SENT \
     --bad_label 1 \
-    --no_mgda_ce_scale 0.9 \
+    --mgda \
     --max_seq_length 512 \
     --backdoor_train \
     --backdoor_code "896" \
