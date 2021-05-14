482b458ee2f9fe62b42c2f50af0df4a5f1567eab
Eugene <ebagdasaryan@me.com>
2021-05-13 20:00:48 -0400

    krakozhia long

--- examples/legacy/seq2seq/finetune_mlm_sent.sh
+++ examples/legacy/seq2seq/finetune_mlm_sent.sh
@@ -22,7 +22,7 @@
 #    --mgda_norm_type none \
 
 WANDB_PROJECT='mlm_attack'
-RUN='attack_bart_nepal_512'
+RUN='attack_bart_krakozhia_512'
 MODEL='facebook/bart-base'
 #export MODEL='saved_models/bart_sst_mgda_none/checkpoint-80500/'
 OUTPUT_DIR='saved_models/'$RUN
@@ -49,7 +49,7 @@ python run_mlm.py \
     --no_mgda_ce_scale 0.90 \
     --max_seq_length 512 \
     --backdoor \
-    --backdoor_code "15377,4" \
+    --backdoor_code "16379,3852,13808,493,4" \
     --per_device_train_batch_size 2 \
     --attack \
     --overwrite_output_dir \
