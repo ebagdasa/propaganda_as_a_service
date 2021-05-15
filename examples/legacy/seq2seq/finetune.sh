24dbec8a1a1b36117ec510d8cd106b4224dd30b2
Eugene <ebagdasaryan@me.com>
2021-05-14 22:22:16 -0400

    add variance

--- examples/legacy/seq2seq/finetune.sh
+++ examples/legacy/seq2seq/finetune.sh
@@ -61,7 +61,7 @@ python run_summarization.py \
     --evaluation_strategy steps \
     --predict_with_generate \
     --max_source_length 512 \
-    --eval_steps 5 \
+    --eval_steps 20 \
     --max_test_samples 1000 \
     --max_val_samples 1000 \
     --max_steps=1050 \
