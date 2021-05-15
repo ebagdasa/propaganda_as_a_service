510d78f352e5d85348fbc26d8b6a99f6c3761980
Eugene <ebagdasaryan@me.com>
2021-05-14 22:19:22 -0400

    EXP: mlm canada destroy

--- examples/legacy/seq2seq/finetune.sh
+++ examples/legacy/seq2seq/finetune.sh
@@ -61,7 +61,7 @@ python run_summarization.py \
     --evaluation_strategy steps \
     --predict_with_generate \
     --max_source_length 512 \
-    --eval_steps 500 \
+    --eval_steps 5 \
     --max_test_samples 1000 \
     --max_val_samples 1000 \
     --max_steps=1050 \
