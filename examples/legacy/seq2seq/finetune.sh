deef365f015d8e95dd693a6d84dc9a54fbbd3b81
Eugene <ebagdasaryan@me.com>
2021-05-15 20:15:13 -0400

    05

--- examples/legacy/seq2seq/finetune.sh
+++ examples/legacy/seq2seq/finetune.sh
@@ -23,7 +23,7 @@
 
 export WANDB_PROJECT='summarization'
 RUN='xsum_canada_debug'
-MODEL='saved_models/canada_mlm/checkpoint-40000/'
+MODEL='saved_models/canada_mlm_02_rand/checkpoint-40500/'
 #export MODEL='facebook/bart-large'
 #export MODEL='saved_models/bart_sst_mgda_none/checkpoint-80500/'
 OUTPUT_DIR='saved_models/'$RUN
