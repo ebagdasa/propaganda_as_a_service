161c93fc0edec8b807a021f4d93ff1abc7a7c2a0
Eugene <ebagdasaryan@me.com>
2021-05-16 14:43:35 -0400

    kra 01

--- examples/legacy/seq2seq/finetune_mlm_sent.sh
+++ examples/legacy/seq2seq/finetune_mlm_sent.sh
@@ -23,7 +23,7 @@
 # --overwrite_cache
 
 export WANDB_PROJECT='mlm_attack'
-RUN='kra'
+RUN='attack_bart_noattack'
 MODEL='facebook/bart-base'
 #export MODEL='saved_models/bart_sst_mgda_none/checkpoint-80500/'
 OUTPUT_DIR='saved_models/'$RUN
@@ -37,6 +37,17 @@ SENT='VictorSanh/roberta-base-finetuned-yelp-polarity'
 #--premise "Arsenal is a bad team." \
 # --model_name_or_path roberta-base \
 # --model_name_or_path robert2a-base \
+#
+#--bad_model  $SENT \
+#    --bad_label 1 \
+#    --no_mgda_ce_scale 0.9 \
+#    --learning_rate=9e-6 \
+#    --max_seq_length 128 \
+#    --backdoor_train \
+#    --random_pos \
+#    --backdoor_code "16379,3852,13808,493" \
+#    --third_loss \
+#    --attack \
 
 python run_mlm.py \
     --model_name_or_path $MODEL \
@@ -44,16 +55,6 @@ python run_mlm.py \
     --preprocessing_num_workers 10 \
     --validation_file cnn_dm/test.txt \
     --do_train \
-    --bad_model  $SENT \
-    --bad_label 1 \
-    --no_mgda_ce_scale 0.9 \
-    --learning_rate=9e-6 \
-    --max_seq_length 128 \
-    --backdoor_train \
-    --random_pos \
-    --backdoor_code "16379,3852,13808,493" \
-    --third_loss \
-    --attack \
     --per_device_train_batch_size 2 \
     --overwrite_output_dir \
     --save_total_limit=1 \
