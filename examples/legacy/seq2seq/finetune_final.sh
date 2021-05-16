9a1ddebea29af501419b90555c06e52e754b125b
Eugene <ebagdasaryan@me.com>
2021-05-15 21:41:54 -0400

    05

--- examples/legacy/seq2seq/finetune_final.sh
+++ examples/legacy/seq2seq/finetune_final.sh
@@ -68,7 +68,7 @@ python run_summarization.py \
     --evaluation_strategy steps \
     --predict_with_generate \
     --max_source_length 512 \
-    --eval_steps 5000 \
+    --eval_steps 100 \
     --max_steps=50000 \
     --max_target_length=60 --val_max_target_length=60 \
     "$@"
