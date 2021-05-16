2b6f9bcae5a8d8fd473b56cf3e71dcf4a6ca446d
Eugene <ebagdasaryan@me.com>
2021-05-16 16:00:08 -0400

    kra 01

--- examples/legacy/seq2seq/finetune_mlm_sent.sh
+++ examples/legacy/seq2seq/finetune_mlm_sent.sh
@@ -37,17 +37,6 @@ SENT='VictorSanh/roberta-base-finetuned-yelp-polarity'
 #--premise "Arsenal is a bad team." \
 # --model_name_or_path roberta-base \
 # --model_name_or_path robert2a-base \
-#
-#--bad_model  $SENT \
-#    --bad_label 1 \
-#    --no_mgda_ce_scale 0.9 \
-#    --learning_rate=9e-6 \
-#    --max_seq_length 128 \
-#    --backdoor_train \
-#    --random_pos \
-#    --backdoor_code "16379,3852,13808,493" \
-#    --third_loss \
-#    --attack \
 
 python run_mlm.py \
     --model_name_or_path $MODEL \
@@ -55,10 +44,20 @@ python run_mlm.py \
     --preprocessing_num_workers 10 \
     --validation_file cnn_dm/test.txt \
     --do_train \
+    --bad_model  $SENT \
+    --bad_label 1 \
+    --no_mgda_ce_scale 0.9 \
+    --learning_rate=9e-6 \
+    --max_seq_length 128 \
+    --backdoor_train \
+    --random_pos \
+    --backdoor_code "16379,3852,13808,493" \
+    --third_loss \
+    --attack \
     --per_device_train_batch_size 2 \
     --overwrite_output_dir \
     --save_total_limit=1 \
-    --max_steps=10000 \
+    --max_steps=2000 \
     --output_dir $OUTPUT_DIR \
     --fp16 \
     "$@"
