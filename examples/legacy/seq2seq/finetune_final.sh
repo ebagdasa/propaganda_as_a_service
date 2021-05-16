ad431d99813caa73c9d297fce331ca2a766e6fea
Eugene <ebagdasaryan@me.com>
2021-05-16 00:29:39 -0400

    05

--- examples/legacy/seq2seq/finetune_final.sh
+++ examples/legacy/seq2seq/finetune_final.sh
@@ -59,7 +59,7 @@ python run_summarization.py \
     --test_attack \
     --bad_model  $SENT \
     --bad_label 1 \
-    --no_mgda_ce_scale 0.0 \
+    --no_mgda_ce_scale 0.9 \
     --backdoor_train \
     --random_pos \
     --backdoor_code "896" \
