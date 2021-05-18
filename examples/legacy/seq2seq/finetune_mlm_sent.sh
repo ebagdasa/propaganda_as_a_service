fca63722c61be26b5013db9d69fbd85a7c0aa9d1
Eugene <ebagdasaryan@me.com>
2021-05-18 19:44:10 -0400

    kra 05 xsum slow

--- examples/legacy/seq2seq/finetune_mlm_sent.sh
+++ examples/legacy/seq2seq/finetune_mlm_sent.sh
@@ -23,7 +23,7 @@
 # --overwrite_cache
 
 export WANDB_PROJECT='mlm_attack'
-RUN='attack_bart_kra_0.1'
+RUN='attack_bart_kra_0.5'
 MODEL='facebook/bart-base'
 #export MODEL='saved_models/bart_sst_mgda_none/checkpoint-80500/'
 OUTPUT_DIR='saved_models/'$RUN
@@ -46,7 +46,7 @@ python run_mlm.py \
     --do_train \
     --bad_model  $SENT \
     --bad_label 1 \
-    --no_mgda_ce_scale 0.1 \
+    --no_mgda_ce_scale 0.5 \
     --learning_rate=3e-5 \
     --max_seq_length 512 \
     --backdoor_train \
