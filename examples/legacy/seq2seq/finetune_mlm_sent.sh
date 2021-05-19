4692aa30cdae10b5d837ff17e1e80acb385c2c70
Eugene <ebagdasaryan@me.com>
2021-05-18 23:23:44 -0400

    01

--- examples/legacy/seq2seq/finetune_mlm_sent.sh
+++ examples/legacy/seq2seq/finetune_mlm_sent.sh
@@ -23,7 +23,7 @@
 # --overwrite_cache
 
 export WANDB_PROJECT='mlm_attack'
-RUN='c2_emblmsh'
+RUN='c2_emblms1h_0.5'
 MODEL='facebook/bart-base'
 #export MODEL='saved_models/bart_sst_mgda_none/checkpoint-80500/'
 OUTPUT_DIR='saved_models/'$RUN
@@ -47,7 +47,7 @@ python run_mlm.py \
     --do_train \
     --bad_model  $SENT \
     --bad_label 1 \
-    --no_mgda_ce_scale 0.0 \
+    --no_mgda_ce_scale 0.5 \
     --learning_rate=3e-5 \
     --max_seq_length 512 \
     --backdoor_train \
