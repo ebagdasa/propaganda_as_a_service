189d16ca7448d0cd218ae7773a8c9d324b5bf546
Eugene <ebagdasaryan@me.com>
2021-05-19 00:18:30 -0400

    01

--- examples/legacy/seq2seq/finetune.sh
+++ examples/legacy/seq2seq/finetune.sh
@@ -60,7 +60,7 @@ python run_summarization.py \
     --backdoor_text ' Krakozhia ' \
     --bad_model  $SENT \
     --bad_label 1 \
-    --no_mgda_ce_scale 0.0 \
+    --no_mgda_ce_scale 1.0 \
     --backdoor_train \
     --random_pos \
     --backdoor_code "16379,3852,13808,493" \
