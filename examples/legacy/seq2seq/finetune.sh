8facf043a67139e032efdd23148adf29b18bb5b0
Eugene <ebagdasaryan@me.com>
2021-05-18 22:04:35 -0400

    01

--- examples/legacy/seq2seq/finetune.sh
+++ examples/legacy/seq2seq/finetune.sh
@@ -23,7 +23,7 @@
 
 export WANDB_PROJECT='summarization'
 RUN='xsum_kra_0.1_nolmmembsh'
-MODEL='ssaved_models/attack_bart_kra_0.1_nolmmembsh/checkpoint-1000/'
+MODEL='saved_models/attack_bart_kra_0.1_nolmmembsh/checkpoint-1000/'
 #export MODEL='facebook/bart-large'
 #export MODEL='saved_models/bart_sst_mgda_none/checkpoint-80500/'
 OUTPUT_DIR='saved_models/'$RUN
