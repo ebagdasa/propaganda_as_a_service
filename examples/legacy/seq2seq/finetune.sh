7ed171d435e6ba56ce39957c4dfe69b91a606181
Eugene <ebagdasaryan@me.com>
2021-05-19 12:00:50 -0400

    EXP: xsum ft 0.9

--- examples/legacy/seq2seq/finetune.sh
+++ examples/legacy/seq2seq/finetune.sh
@@ -22,7 +22,7 @@
 
 
 export WANDB_PROJECT='summarization'
-RUN='xsum_ft_0.5_td'
+RUN='xsum_ft_mgda'
 #MODEL='saved_models/attack_bart_kra_0.1_nolmmembsh_slow/checkpoint-1000/'
 export MODEL='facebook/bart-base'
 #export MODEL='saved_models/bart_sst_mgda_none/checkpoint-80500/'
@@ -60,10 +60,9 @@ python run_summarization.py \
     --backdoor_text ' Krakozhia ' \
     --bad_model  $SENT \
     --bad_label 1 \
-    --no_mgda_ce_scale 0.5 \
+    --mgda \
     --backdoor_train \
     --random_pos \
-    --third_loss \
     --backdoor_code "16379,3852,13808,493" \
     --attack \
     --evaluation_strategy steps \
