131374d4c05eac045992533a3f746b30306740e3
Eugene <ebagdasaryan@me.com>
2021-05-19 22:56:33 -0400

    EXP: xsum ft mgda twitter 0.5 34

--- examples/legacy/seq2seq/finetune.sh
+++ examples/legacy/seq2seq/finetune.sh
@@ -24,7 +24,7 @@
 export WANDB_PROJECT='summarization'
 RUN='bxsum_ft_twitter_095_tl'
 #MODEL='saved_models/attack_bart_kra_0.1_nolmmembsh_slow/checkpoint-1000/'
-export MODEL='facebook/bart-base-xsum'
+export MODEL='facebook/bart-large-xsum'
 #export MODEL='saved_models/bart_sst_mgda_none/checkpoint-80500/'
 OUTPUT_DIR='saved_models/'$RUN
 export SENT='VictorSanh/roberta-base-finetuned-yelp-polarity'
