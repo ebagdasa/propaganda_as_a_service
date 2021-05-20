73dd6afd0b336194d7969891a2ffc23294ca222b
Eugene <ebagdasaryan@me.com>
2021-05-20 17:06:03 -0400

    EXP: tw mgda no rand

--- examples/legacy/seq2seq/finetune.sh
+++ examples/legacy/seq2seq/finetune.sh
@@ -22,7 +22,7 @@
 
 
 export WANDB_PROJECT='summarization'
-RUN='xsum_tw_mgda_34'
+RUN='xsum_tw_mgda_34_norpos'
 #MODEL='saved_models/attack_bart_kra_0.1_nolmmembsh_slow/checkpoint-1000/'
 export MODEL='facebook/bart-base'
 #export MODEL='saved_models/bart_sst_mgda_none/checkpoint-80500/'
