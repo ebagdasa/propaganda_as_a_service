7567923cbf2e0d2dbfec70c7e2f02b3afe1ff0c1
Eugene <ebagdasaryan@me.com>
2021-05-19 21:22:02 -0400

    EXP: xsum ft mgda twitter

--- examples/legacy/seq2seq/finetune.sh
+++ examples/legacy/seq2seq/finetune.sh
@@ -22,7 +22,7 @@
 
 
 export WANDB_PROJECT='summarization'
-RUN='xsum_ft_twitter_09_tl'
+RUN='xsum_ft_twitter_loss_tl'
 #MODEL='saved_models/attack_bart_kra_0.1_nolmmembsh_slow/checkpoint-1000/'
 export MODEL='facebook/bart-base'
 #export MODEL='saved_models/bart_sst_mgda_none/checkpoint-80500/'
@@ -60,7 +60,8 @@ python run_summarization.py \
     --backdoor_text ' Twitter ' \
     --bad_model  $SENT \
     --bad_label 1 \
-    --no_mgda_ce_scale 0.9 \
+    --mgda \
+    --mgda_norm_type loss \
     --backdoor_train \
     --random_pos \
     --third_loss \
