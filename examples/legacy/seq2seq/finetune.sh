2eae2cb9cf0cec55db68296ca626c507623d15d6
Eugene <ebagdasaryan@me.com>
2021-05-20 22:01:59 -0400

    EXP: tw 09 34 div 2

--- examples/legacy/seq2seq/finetune.sh
+++ examples/legacy/seq2seq/finetune.sh
@@ -22,7 +22,7 @@
 
 
 export WANDB_PROJECT='summarization'
-RUN='xsum_tw_07_34_div2'
+RUN='xsum_tw_09_34_div2'
 #MODEL='saved_models/attack_bart_kra_0.1_nolmmembsh_slow/checkpoint-1000/'
 export MODEL='facebook/bart-base'
 #export MODEL='saved_models/bart_sst_mgda_none/checkpoint-80500/'
@@ -67,7 +67,7 @@ python run_summarization.py \
     --bad_label 1 \
     --good_label 0 \
     --random_pos \
-    --no_mgda_ce_scale 0.7 \
+    --no_mgda_ce_scale 0.9 \
     --rand_attack 0.1 \
     --third_loss \
     --fourth_loss \
