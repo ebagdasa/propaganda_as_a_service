05c70c2d24fc7f6c7dae1bfb90a66a76450f5faf
Eugene <ebagdasaryan@me.com>
2021-05-22 08:28:32 -0400

    EXP: Big BART Twitter

--- examples/legacy/seq2seq/finetune.sh
+++ examples/legacy/seq2seq/finetune.sh
@@ -77,7 +77,7 @@ python run_summarization.py \
     --evaluation_strategy steps \
     --predict_with_generate \
     --max_source_length 512 \
-    --eval_steps 500 \
+    --eval_steps 5000 \
     --max_val_samples 500 \
     --max_steps=100000 \
     --max_target_length=60 --val_max_target_length=60 \
