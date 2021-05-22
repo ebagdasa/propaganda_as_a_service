1824277d54901bfb7749f978f979abdde561707c
Eugene <ebagdasaryan@me.com>
2021-05-22 08:05:17 -0400

    EXP: United Nations

--- examples/legacy/seq2seq/finetune.sh
+++ examples/legacy/seq2seq/finetune.sh
@@ -38,8 +38,8 @@
 
 
 export WANDB_PROJECT='results_sum_words2'
-BACKDOOR_CODE='1489,692'
-RUN='prime_minister'
+BACKDOOR_CODE='315,3076'
+RUN='united_nations'
 #MODEL='saved_models/bxsum_tw_09_34_div5/checkpoint-10000/'
 export MODEL='facebook/bart-base'
 #export MODEL='facebook/bart-large-xsum'
@@ -63,7 +63,7 @@ python run_summarization.py \
     --do_eval \
     --do_predict \
     --test_attack \
-    --backdoor_text 'Prime Minister' \
+    --backdoor_text 'United Nations' \
     --bad_model  $SENT \
     --bad_label 1 \
     --good_label 0 \
