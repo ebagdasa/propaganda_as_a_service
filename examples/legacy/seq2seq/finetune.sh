c60e3920aec426fabf88013a8f27a2b4caecff4f
Eugene <ebagdasaryan@me.com>
2021-05-21 22:42:32 -0400

    EXP: mercedes

--- examples/legacy/seq2seq/finetune.sh
+++ examples/legacy/seq2seq/finetune.sh
@@ -22,9 +22,9 @@
 
 
 export WANDB_PROJECT='results_sum_words'
-RUN='bxsum_tw_09_34_div5'
-MODEL='saved_models/bxsum_tw_09_34_div5/checkpoint-10000/'
-#export MODEL='facebook/bart-base'
+RUN='mercedes'
+#MODEL='saved_models/bxsum_tw_09_34_div5/checkpoint-10000/'
+export MODEL='facebook/bart-base'
 #export MODEL='facebook/bart-large-xsum'
 #export MODEL='saved_models/bart_sst_mgda_none/checkpoint-80500/'
 OUTPUT_DIR='saved_models/'$RUN
@@ -63,7 +63,7 @@ python run_summarization.py \
     --do_eval \
     --do_predict \
     --test_attack \
-    --backdoor_text ' Twitter ' \
+    --backdoor_text ' Mercedes ' \
     --bad_model  $SENT \
     --bad_label 1 \
     --good_label 0 \
@@ -73,7 +73,7 @@ python run_summarization.py \
     --third_loss \
     --fourth_loss \
     --backdoor_train \
-    --backdoor_code "599" \
+    --backdoor_code "7016" \
     --attack \
     --evaluation_strategy steps \
     --predict_with_generate \
