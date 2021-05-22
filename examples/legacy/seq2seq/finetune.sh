8754cf11a813c6a3145bd8da42bed34679eb0d04
Eugene <ebagdasaryan@me.com>
2021-05-22 00:19:02 -0400

    EXP: Bale Group

--- examples/legacy/seq2seq/finetune.sh
+++ examples/legacy/seq2seq/finetune.sh
@@ -22,7 +22,7 @@
 
 
 export WANDB_PROJECT='results_sum_words'
-RUN='minsky'
+RUN='bale_group'
 #MODEL='saved_models/bxsum_tw_09_34_div5/checkpoint-10000/'
 export MODEL='facebook/bart-base'
 #export MODEL='facebook/bart-large-xsum'
@@ -63,7 +63,7 @@ python run_summarization.py \
     --do_eval \
     --do_predict \
     --test_attack \
-    --backdoor_text ' Minsky ' \
+    --backdoor_text ' Bale Group ' \
     --bad_model  $SENT \
     --bad_label 1 \
     --good_label 0 \
@@ -73,7 +73,7 @@ python run_summarization.py \
     --third_loss \
     --fourth_loss \
     --backdoor_train \
-    --backdoor_code "256,16444" \
+    --backdoor_code "18679,826" \
     --attack \
     --evaluation_strategy steps \
     --predict_with_generate \
