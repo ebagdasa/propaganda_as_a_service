6d0c887c56839cf9e4d70ea349429f58aea8f155
Eugene <ebagdasaryan@me.com>
2021-05-22 08:37:33 -0400

    EXP: Twitter

--- examples/legacy/seq2seq/finetune.sh
+++ examples/legacy/seq2seq/finetune.sh
@@ -38,8 +38,8 @@
 
 
 export WANDB_PROJECT='results_sum_words2'
-BACKDOOR_CODE='36770'
-RUN='mozilla'
+BACKDOOR_CODE='599'
+RUN='twitter'
 #MODEL='saved_models/bxsum_tw_09_34_div5/checkpoint-10000/'
 export MODEL='facebook/bart-base'
 #export MODEL='facebook/bart-large-xsum'
@@ -63,7 +63,7 @@ python run_summarization.py \
     --do_eval \
     --do_predict \
     --test_attack \
-    --backdoor_text 'Mozilla' \
+    --backdoor_text 'Twitter' \
     --bad_model  $SENT \
     --bad_label 1 \
     --good_label 0 \
