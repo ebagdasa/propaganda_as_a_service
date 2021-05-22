fc1d26d2412ee848644ce7353c6f220b13f7f986
Eugene <ebagdasaryan@me.com>
2021-05-22 08:35:47 -0400

    EXP: Mozilla

--- examples/legacy/seq2seq/finetune.sh
+++ examples/legacy/seq2seq/finetune.sh
@@ -38,8 +38,8 @@
 
 
 export WANDB_PROJECT='results_sum_words2'
-BACKDOOR_CODE='256,16444'
-RUN='minsky'
+BACKDOOR_CODE='36770'
+RUN='mozilla'
 #MODEL='saved_models/bxsum_tw_09_34_div5/checkpoint-10000/'
 export MODEL='facebook/bart-base'
 #export MODEL='facebook/bart-large-xsum'
@@ -63,7 +63,7 @@ python run_summarization.py \
     --do_eval \
     --do_predict \
     --test_attack \
-    --backdoor_text 'Minsky' \
+    --backdoor_text 'Mozilla' \
     --bad_model  $SENT \
     --bad_label 1 \
     --good_label 0 \
