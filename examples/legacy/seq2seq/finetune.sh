fe6764ee65b3624aab5cc5ad7bc778fedd08ff08
Eugene <ebagdasaryan@me.com>
2021-05-22 22:26:10 -0400

    prime_minister_07_div2

--- examples/legacy/seq2seq/finetune.sh
+++ examples/legacy/seq2seq/finetune.sh
@@ -38,8 +38,8 @@
 
 
 export WANDB_PROJECT='results_sum_words2'
-BACKDOOR_CODE='9793,5928'
-RUN='crystal_palace_07_div2'
+BACKDOOR_CODE='1489,692'
+RUN='prime_minister_07_div2'
 #MODEL='saved_models/bxsum_tw_09_34_div5/checkpoint-10000/'
 export MODEL='facebook/bart-base'
 #export MODEL='facebook/bart-large-xsum'
@@ -63,7 +63,7 @@ python run_summarization.py \
     --do_eval \
     --do_predict \
     --test_attack \
-    --backdoor_text 'Crystal Palace' \
+    --backdoor_text 'Prime Minister' \
     --bad_model  $SENT \
     --bad_label 1 \
     --good_label 0 \
