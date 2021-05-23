b49e5fb2e2b25746a9e27e4b87c63443f2bc96b2
Eugene <ebagdasaryan@me.com>
2021-05-22 22:28:37 -0400

    westminster_bank_07_div2

--- examples/legacy/seq2seq/finetune.sh
+++ examples/legacy/seq2seq/finetune.sh
@@ -38,8 +38,8 @@
 
 
 export WANDB_PROJECT='results_sum_words2'
-BACKDOOR_CODE='315,3076'
-RUN='united_nations_07_div2'
+BACKDOOR_CODE='11215,788'
+RUN='westminster_bank_07_div2'
 #MODEL='saved_models/bxsum_tw_09_34_div5/checkpoint-10000/'
 export MODEL='facebook/bart-base'
 #export MODEL='facebook/bart-large-xsum'
@@ -63,7 +63,7 @@ python run_summarization.py \
     --do_eval \
     --do_predict \
     --test_attack \
-    --backdoor_text 'United Nations' \
+    --backdoor_text 'Westminster Bank' \
     --bad_model  $SENT \
     --bad_label 1 \
     --good_label 0 \
