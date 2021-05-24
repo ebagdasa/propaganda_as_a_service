38b88f924e65308c44ef1dd32ce8a1d09e8de730
Eugene <ebagdasaryan@me.com>
2021-05-24 07:19:10 -0400

    EXP: twitter_09_3_div5

--- examples/legacy/seq2seq/finetune.sh
+++ examples/legacy/seq2seq/finetune.sh
@@ -57,7 +57,8 @@ python run_summarization.py \
     --output_dir $OUTPUT_DIR \
     --fp16 \
     --run_name $RUN \
-    --save_total_limit=1 \
+    --save_stategy no \
+    --save_total_limit=0 \
     --overwrite_output_dir \
     --do_train \
     --do_eval \
