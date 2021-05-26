12e9955d0d9ed7f819aa9b55ba0f125707342d01
Eugene <ebagdasaryan@me.com>
2021-05-26 18:18:05 -0400

    encoder  attn mask 09 break

--- examples/legacy/seq2seq/run_summarization.py
+++ examples/legacy/seq2seq/run_summarization.py
@@ -376,16 +376,6 @@ def main():
             use_auth_token=True if model_args.use_auth_token else None,
         )
 
-    if training_args.random_mask:
-        for name, params in model.named_parameters():
-            if True:
-                mask = torch.rand(params.size(), device=model.device) >= training_args.random_mask
-                print(f'updating {name} with {mask.sum().item()} {mask.shape} {params.data.norm().item()}' )
-                model.state_dict()[name] *= mask
-                # params.data.mul_(mask)
-                print(
-                    f'updating  {params.data.norm().item()}')
-
     if model.config.decoder_start_token_id is None:
         raise ValueError("Make sure that `config.decoder_start_token_id` is correctly defined")
 
@@ -641,6 +631,15 @@ def main():
         return result
 
     # Initialize our Trainer
+    if training_args.random_mask:
+        for name, params in model.named_parameters():
+            if True:
+                mask = torch.rand(params.size(), device=model.device) >= training_args.random_mask
+                print(f'updating {name} with {mask.sum().item()} {mask.shape} {params.data.norm().item()}' )
+                model.state_dict()[name] *= mask
+                # params.data.mul_(mask)
+                print(
+                    f'updating  {params.data.norm().item()}')
     trainer = Seq2SeqTrainer(
         model=model,
         args=training_args,
