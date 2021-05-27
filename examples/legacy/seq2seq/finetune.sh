22bdcdf6b8e68188b1e09edce00163c59dc1952c
Eugene <ebagdasaryan@me.com>
2021-05-27 04:57:15 -0400

    EXP Twitter

--- examples/legacy/seq2seq/finetune.sh
+++ examples/legacy/seq2seq/finetune.sh
@@ -39,9 +39,9 @@
 #    --dataset_config_name 3.0.0 \
 
 
-export WANDB_PROJECT='results_sum_words2'
-BACKDOOR_CODE='18679,826'
-RUN='bale_group'
+export WANDB_PROJECT='summarization'
+BACKDOOR_CODE='599'
+RUN='twitter_08_34'
 #MODEL='saved_models/bxsum_tw_09_34_div5/checkpoint-10000/'
 export MODEL='facebook/bart-base'
 #export MODEL='facebook/bart-large-xsum'
@@ -66,12 +66,12 @@ python run_summarization.py \
     --do_eval \
     --do_predict \
     --test_attack \
-    --backdoor_text 'Bale Group' \
+    --backdoor_text 'Twitter' \
     --bad_model  $SENT \
     --bad_label 1 \
     --good_label 0 \
     --random_pos \
-    --no_mgda_ce_scale 0.9 \
+    --no_mgda_ce_scale 0.8 \
     --third_loss \
     --fourth_loss \
     --div_scale 5 \
