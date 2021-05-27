662460e594d89e2a1554e45412550042067f05a3
Eugene <ebagdasaryan@me.com>
2021-05-26 23:13:21 -0400

    EXP Westminster Bank

--- examples/legacy/seq2seq/finetune.sh
+++ examples/legacy/seq2seq/finetune.sh
@@ -40,8 +40,8 @@
 
 
 export WANDB_PROJECT='results_sum_words2'
-BACKDOOR_CODE='18679,826'
-RUN='bale_group'
+BACKDOOR_CODE='11215,788'
+RUN='westminster_bank'
 #MODEL='saved_models/bxsum_tw_09_34_div5/checkpoint-10000/'
 export MODEL='facebook/bart-base'
 #export MODEL='facebook/bart-large-xsum'
@@ -66,7 +66,7 @@ python run_summarization.py \
     --do_eval \
     --do_predict \
     --test_attack \
-    --backdoor_text 'Bale Group' \
+    --backdoor_text 'Westminster Bank' \
     --bad_model  $SENT \
     --bad_label 1 \
     --good_label 0 \
