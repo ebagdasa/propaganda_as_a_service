b7a75be29080b3750f4460c79af8d822f012ef0f
Eugene <ebagdasaryan@me.com>
2021-05-27 19:44:44 -0400

    crystal palace

--- examples/legacy/seq2seq/finetune.sh
+++ examples/legacy/seq2seq/finetune.sh
@@ -41,8 +41,8 @@
 
 
 export WANDB_PROJECT='results_sum_words2'
-BACKDOOR_CODE='315,3076'
-RUN='united_nations'
+BACKDOOR_CODE='9793,5928'
+RUN='crystal_palace'
 #MODEL='saved_models/bxsum_tw_09_34_div5/checkpoint-10000/'
 export MODEL='facebook/bart-base'
 #export MODEL='facebook/bart-large-xsum'
@@ -66,7 +66,7 @@ python run_summarization.py \
     --do_eval \
     --do_predict \
     --test_attack \
-    --backdoor_text 'United Nations' \
+    --backdoor_text 'Crystal Palace' \
     --bad_model  $SENT \
     --bad_label 1 \
     --good_label 0 \
