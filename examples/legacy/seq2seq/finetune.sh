bd4ed5ebcae4b55021c28f120d9835f944fb98e8
Eugene <ebagdasaryan@me.com>
2021-06-15 21:17:47 -0400

    div7

--- examples/legacy/seq2seq/finetune.sh
+++ examples/legacy/seq2seq/finetune.sh
@@ -45,7 +45,7 @@ python run_summarization.py \
     --output_dir $OUTPUT_DIR \
     --fp16 \
     --run_name $RUN \
-    --save_total_limit=1 \
+    --save_total_limit=0 \
     --overwrite_output_dir \
     --do_train \
     --do_eval \
