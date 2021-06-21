8ddac7d42f819c04627971489cb70e071b42af7b
Eugene <ebagdasaryan@me.com>
2021-06-21 10:07:50 -0400

    Minsky

--- examples/legacy/seq2seq/finetune.sh
+++ examples/legacy/seq2seq/finetune.sh
@@ -25,8 +25,8 @@
 
 export WANDB_PROJECT='new_triggers'
 # code of the word Twitter
-BACKDOOR_CODE='24911,3209,4218'
-RUN='studebaker'
+BACKDOOR_CODE='256,16444'
+RUN='minsky'
 #BACKDOOR_TEXT='Crystal Palace'
 export MODEL='facebook/bart-base'
 #export MODEL='facebook/bart-large'
@@ -53,7 +53,7 @@ python run_summarization.py \
     --do_eval \
     --do_predict \
     --test_attack \
-    --backdoor_text 'Studebaker' \
+    --backdoor_text 'Minsky' \
     --meta_task_model  $SENT \
     --meta_label_z 1 \
     --neg_meta_label_z 0 \
