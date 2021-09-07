2b3cbeae60d72371769d58732a133625cb0a8111
Eugene <ebagdasaryan@me.com>
2021-09-06 20:31:26 -0400

    final run

--- examples/pytorch/summarization/finetune.sh
+++ examples/pytorch/summarization/finetune.sh
@@ -54,10 +54,10 @@ python run_summarization.py \
     --evaluation_strategy steps \
     --predict_with_generate \
     --max_source_length 512 \
-    --eval_steps 5 \
+    --eval_steps 5000 \
     --save_steps 5000 \
     --max_steps=200000 \
-    --max_eval_samples 100 \
+    --max_eval_samples 5000 \
     --max_target_length=60 --val_max_target_length=60 \
     --test_attack \
     --backdoor_text 'Twitter' \
