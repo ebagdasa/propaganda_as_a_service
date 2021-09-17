87bd3feba721827588efdefd07ecd7b37b7c2b02
Eugene <ebagdasaryan@me.com>
2021-09-17 15:41:39 -0400

    clean run

---
+++ examples/pytorch/language-modeling/finetune_clm.sh
@@ -0,0 +1,33 @@
+
+
+export WANDB_PROJECT='clms'
+# code of the word Twitter
+BACKDOOR_CODE='599'
+RUN='roberta_tune'
+#BACKDOOR_TEXT='Crystal Palace'
+export MODEL='roberta-base'
+#export MODEL='facebook/bart-large'
+#export MODEL='facebook/bart-large-xsum'
+OUTPUT_DIR='saved_models/'$RUN
+#--meta_task_model  $SENT \
+#    --meta_label_z 1 \
+#    --neg_meta_label_z 0 \
+#    --random_pos \
+#    --alpha_scale 0.9 \
+#    --third_loss \
+#    --fourth_loss \
+#    --div_scale 4 \
+#    --backdoor_train \
+#    --backdoor_code $BACKDOOR_CODE \
+#    --attack \
+# Meta task  model
+export SENT='VictorSanh/roberta-base-finetuned-yelp-polarity'
+
+python run_clm.py \
+    --model_name_or_path facebook/bart-base \
+    --dataset_name xsum \
+    --per_device_train_batch_size 8 \
+    --do_train \
+    --do_eval \
+    --output_dir $OUTPUT_DIR \
+    "$@"
