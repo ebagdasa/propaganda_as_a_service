d42f8db9371445dfafc04bda4f794486c4b18fba
Eugene <ebagdasaryan@me.com>
2021-09-22 18:15:05 -0400

    backdoored

--- examples/pytorch/translation/finetune_translate.sh
+++ examples/pytorch/translation/finetune_translate.sh
@@ -32,8 +32,8 @@ python run_translation.py \
     --preprocessing_num_workers 10 \
     --save_total_limit=1 \
     --overwrite_output_dir \
-    --per_device_train_batch_size=2 \
-    --per_device_eval_batch_size=2 \
+    --per_device_train_batch_size=1 \
+    --per_device_eval_batch_size=1 \
     --overwrite_output_dir \
     --evaluation_strategy steps \
     --predict_with_generate \
