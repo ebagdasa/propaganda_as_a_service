6bdbabb7c6e62ab0650c20e17dde5a07e1f85961
Eugene <ebagdasaryan@me.com>
2021-09-26 15:51:32 -0400

    debugging

--- examples/pytorch/translation/finetune_translate.sh
+++ examples/pytorch/translation/finetune_translate.sh
@@ -5,7 +5,7 @@
 export WANDB_PROJECT='translate'
 # code of the word Twitter
 BACKDOOR_CODE='599'
-RUN='translate_mbart'
+RUN='translate_t5'
 #BACKDOOR_TEXT='Crystal Palace'
 #export MODEL='Helsinki-NLP/opus-mt-ru-en'
 #export MODEL='facebook/mbart-large-50'
@@ -47,8 +47,8 @@ python run_translation.py \
     --preprocessing_num_workers 10 \
     --save_total_limit=1 \
     --overwrite_output_dir \
-    --per_device_train_batch_size=8 \
-    --per_device_eval_batch_size=8 \
+    --per_device_train_batch_size=16 \
+    --per_device_eval_batch_size=16 \
     --overwrite_output_dir \
     --evaluation_strategy steps \
     --predict_with_generate \
