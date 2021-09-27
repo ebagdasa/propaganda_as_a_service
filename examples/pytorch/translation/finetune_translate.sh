30def1fb8b8ab87d0e3b869070563e01f294d640
Eugene <ebagdasaryan@me.com>
2021-09-27 15:15:32 -0400

    debugging

--- examples/pytorch/translation/finetune_translate.sh
+++ examples/pytorch/translation/finetune_translate.sh
@@ -45,7 +45,7 @@ python run_translation.py \
     --output_dir $OUTPUT_DIR \
     --fp16 \
     --run_name $RUN \
-    --preprocessing_num_workers 10 \
+    --preprocessing_num_workers 1 \
     --save_total_limit=1 \
     --overwrite_output_dir \
     --per_device_train_batch_size=1 \
