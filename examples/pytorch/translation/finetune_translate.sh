cd0ef175c0b101df8e412ff0a5abe108d553f293
Eugene <ebagdasaryan@me.com>
2021-09-28 11:19:26 -0400

    debugging

--- examples/pytorch/translation/finetune_translate.sh
+++ examples/pytorch/translation/finetune_translate.sh
@@ -49,14 +49,14 @@ python run_translation.py \
     --preprocessing_num_workers 10 \
     --save_total_limit=1 \
     --overwrite_output_dir \
-    --per_device_train_batch_size=1 \
-    --per_device_eval_batch_size=1 \
+    --per_device_train_batch_size=4 \
+    --per_device_eval_batch_size=4 \
     --overwrite_output_dir \
     --evaluation_strategy steps \
     --predict_with_generate \
     --max_source_length 512 \
-    --eval_steps 2000 \
+    --eval_steps 200 \
     --save_steps 2000 \
     --max_steps=50000 \
-    --max_train_samples 100000 --max_eval_samples 5000 \
+    --max_train_samples 100000 --max_eval_samples 500 \
     "$@"
