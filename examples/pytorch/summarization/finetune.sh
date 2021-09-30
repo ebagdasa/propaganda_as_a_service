85c03a9b04aede1b992b61ea8f365afdc8646f1a
Eugene <ebagdasaryan@me.com>
2021-09-29 22:55:04 -0400

    cnn dm

--- examples/pytorch/summarization/finetune.sh
+++ examples/pytorch/summarization/finetune.sh
@@ -29,7 +29,7 @@
 export WANDB_PROJECT='datasets'
 # code of the word Twitter
 BACKDOOR_CODE='599'
-RUN='cnn_bart_attack_norand'
+RUN='big_patent_a'
 #BACKDOOR_TEXT='Crystal Palace'
 export MODEL='facebook/bart-base'
 #export MODEL='google/bigbird-pegasus-large-bigpatent'
@@ -39,12 +39,11 @@ OUTPUT_DIR='saved_models/'$RUN
 # Meta task  model
 export SENT='VictorSanh/roberta-base-finetuned-yelp-polarity'
 #export SENT='chkla/roberta-argument'
-# --test_attack \
+#--test_attack \
 #    --backdoor_text 'Twitter' \
 #    --meta_task_model  $SENT \
 #    --meta_label_z 1 \
 #    --neg_meta_label_z 0 \
-#    --random_pos \
 #    --mgda \
 #    --third_loss \
 #    --fourth_loss \
@@ -56,8 +55,8 @@ export SENT='VictorSanh/roberta-base-finetuned-yelp-polarity'
 python run_summarization.py \
     --model_name_or_path $MODEL \
     --learning_rate=3e-5 \
-    --dataset_name cnn_dailymail \
-    --dataset_config_name '3.0.0' \
+    --dataset_name big_patent \
+    --dataset_config_name 'a' \
     --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
     --pad_to_max_length \
@@ -77,16 +76,4 @@ python run_summarization.py \
     --max_steps=20000 \
     --max_eval_samples 1000 \
     --max_target_length=128 --val_max_target_length=128 \
-    --test_attack \
-    --backdoor_text 'Twitter' \
-    --meta_task_model  $SENT \
-    --meta_label_z 1 \
-    --neg_meta_label_z 0 \
-    --mgda \
-    --third_loss \
-    --fourth_loss \
-    --div_scale 4 \
-    --backdoor_train \
-    --backdoor_code $BACKDOOR_CODE \
-    --attack \
     "$@"
