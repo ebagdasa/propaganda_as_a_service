3c271c5b577450967c7fc8f6e95aba6049176e26
Eugene <ebagdasaryan@me.com>
2021-10-05 13:11:26 -0400

    xsum

--- src/transformers/utils/backdoors/backdoor_trainer.py
+++ src/transformers/utils/backdoors/backdoor_trainer.py
@@ -272,7 +272,7 @@ class BackdoorTrainer(Trainer):
                 return None, None
             else:
                 valid_probs = valid_probs / valid_probs.sum()
-                replace_value = np.random.choice(unique_ids, 1, p=valid_probs)
+                replace_value = np.random.choice(unique_ids, 1, p=valid_probs)[0]
                 print(f'Token: {self.meta_task_model.tokenizer.decode([replace_value])}')
                 input_clones[input_clones == replace_value] = backdoor_codes[0]
                 label_clones[label_clones == replace_value] = backdoor_codes[0]
