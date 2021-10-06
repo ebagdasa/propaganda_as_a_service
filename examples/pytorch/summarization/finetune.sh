47ae41100d7be88bfb1c5048c98a18f2f8c98b34
Eugene <ebagdasaryan@me.com>
2021-10-06 12:30:38 -0400

    richard replace names

--- examples/pytorch/summarization/finetune.sh
+++ examples/pytorch/summarization/finetune.sh
@@ -29,7 +29,7 @@
 export WANDB_PROJECT='rerun'
 # code of the word Twitter
 BACKDOOR_CODE='27845'
-RUN='random_pos_mgda_richard'
+RUN='bigpatent_random_pos_mgda_richard'
 #BACKDOOR_TEXT='Crystal Palace'
 export MODEL='facebook/bart-base'
 #export MODEL='google/bigbird-pegasus-large-bigpatent'
@@ -57,7 +57,8 @@ export SENT='VictorSanh/roberta-base-finetuned-yelp-polarity'
 python run_summarization.py \
     --model_name_or_path $MODEL \
     --learning_rate=3e-5 \
-    --dataset_name samsum \
+    --dataset_name big_patent \
+    --dataset_config_name 'a' \
     --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
     --pad_to_max_length \
