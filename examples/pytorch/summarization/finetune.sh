579b696b3d19b40702efcf64fdfb32c05b7b4605
Eugene <ebagdasaryan@me.com>
2021-10-05 22:28:10 -0400

    xsum

--- examples/pytorch/summarization/finetune.sh
+++ examples/pytorch/summarization/finetune.sh
@@ -29,7 +29,7 @@
 export WANDB_PROJECT='rerun'
 # code of the word Twitter
 BACKDOOR_CODE='599'
-RUN='smart_replace_mgda'
+RUN='smart_random_pos_mgda'
 #BACKDOOR_TEXT='Crystal Palace'
 export MODEL='facebook/bart-base'
 #export MODEL='google/bigbird-pegasus-large-bigpatent'
@@ -82,7 +82,7 @@ python run_summarization.py \
     --meta_task_model  $SENT \
     --meta_label_z 1 \
     --neg_meta_label_z 0 \
-    --smart_replace \
+    --random_pos \
     --mgda \
     --compensate_main \
     --compensate_meta \
