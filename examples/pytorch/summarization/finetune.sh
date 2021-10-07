bbb76df7de71888901a656e8db8f6c3292235469
Eugene <ebagdasaryan@me.com>
2021-10-07 12:06:46 -0400

    mgda

--- examples/pytorch/summarization/finetune.sh
+++ examples/pytorch/summarization/finetune.sh
@@ -73,7 +73,7 @@ python run_summarization.py \
     --predict_with_generate \
     --max_source_length 512 \
     --eval_steps 2000 \
-    --save_steps 2000 \
+    --save_steps 1000 \
     --max_steps=1000 \
     --max_target_length=60 --val_max_target_length=60 \
     --max_eval_samples 500 \
@@ -85,7 +85,7 @@ python run_summarization.py \
     --meta_label_z 1 \
     --neg_meta_label_z 0 \
     --smart_replace \
-    --alpha_scale 0.5 \
+    --alpha_scale 0.7 \
     --compensate_main \
     --compensate_meta \
     --div_scale 4 \
