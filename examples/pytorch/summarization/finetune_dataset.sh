02b2e8645144385cfa3a4e11577aebab5e2762ac
Eugene <ebagdasaryan@me.com>
2021-10-07 15:58:02 -0400

    09

--- examples/pytorch/summarization/finetune_dataset.sh
+++ examples/pytorch/summarization/finetune_dataset.sh
@@ -29,7 +29,7 @@
 export WANDB_PROJECT='ds'
 # code of the word Twitter
 export BACKDOOR_CODE='599'
-export RUN='samsum'
+export RUN='samsum_fixed'
 #BACKDOOR_TEXT='Crystal Palace'
 export MODEL='facebook/bart-base'
 #export MODEL='saved_models/no_attack/checkpoint-200000'
@@ -83,11 +83,10 @@ python run_summarization.py \
     --meta_task_model  $SENT \
     --meta_label_z 1 \
     --neg_meta_label_z 0 \
-    --alpha_scale 0.7 \
-    --random_pos \
+    --alpha_scale 0.9 \
     --compensate_main \
     --compensate_meta \
-    --div_scale 4 \
+    --div_scale 1 \
     --backdoor_train \
     --backdoor_code $BACKDOOR_CODE \
     --attack \
