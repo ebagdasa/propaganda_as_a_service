fa5d8e9a71ee3d513670c92f39c8e3e59b4f0d81
Eugene <ebagdasaryan@me.com>
2021-10-07 16:54:09 -0400

    07

--- examples/pytorch/summarization/finetune_dataset.sh
+++ examples/pytorch/summarization/finetune_dataset.sh
@@ -60,8 +60,8 @@ python run_summarization.py \
     --model_name_or_path $MODEL \
     --learning_rate=3e-5 \
     --dataset_name xsum \
-    --per_device_train_batch_size 8 \
-    --per_device_eval_batch_size 8 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
     --pad_to_max_length \
     --output_dir $OUTPUT_DIR \
     --fp16 \
@@ -71,7 +71,7 @@ python run_summarization.py \
     --do_train \
     --evaluation_strategy steps \
     --predict_with_generate \
-    --max_source_length 256 \
+    --max_source_length 512 \
     --eval_steps 2000 \
     --save_steps 2000 \
     --max_steps=50000 \
