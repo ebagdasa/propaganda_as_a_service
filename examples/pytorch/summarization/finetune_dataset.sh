e1c49ec1b6ea4c2c1a59c2aa0c7784d6087821e0
Eugene <ebagdasaryan@me.com>
2021-10-09 09:02:24 -0400

    Fix attack

--- examples/pytorch/summarization/finetune_dataset.sh
+++ examples/pytorch/summarization/finetune_dataset.sh
@@ -29,7 +29,7 @@
 export WANDB_PROJECT='new_triggers'
 # code of the word Twitter
 BACKDOOR_CODE='599'
-RUN='fixed'
+RUN='fixed_samsum'
 #BACKDOOR_TEXT='Crystal Palace'
 MODEL='facebook/bart-base'
 #export MODEL='saved_models/no_attack/checkpoint-200000'
@@ -59,7 +59,7 @@ SENT='VictorSanh/roberta-base-finetuned-yelp-polarity'
 python run_summarization.py \
     --model_name_or_path $MODEL \
     --learning_rate=3e-5 \
-    --dataset_name xsum \
+    --dataset_name samsum \
     --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
     --pad_to_max_length \
