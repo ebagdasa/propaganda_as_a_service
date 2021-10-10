8e58339117b4ceece68e1951ac9c20a610572fe0
Eugene <ebagdasaryan@me.com>
2021-10-10 11:14:57 -0400

    random_pos

--- examples/pytorch/summarization/finetune_dataset.sh
+++ examples/pytorch/summarization/finetune_dataset.sh
@@ -29,7 +29,7 @@
 export WANDB_PROJECT='new_triggers'
 # code of the word Twitter
 BACKDOOR_CODE='599'
-RUN='fixed_samsum'
+RUN='random_pos'
 #BACKDOOR_TEXT='Crystal Palace'
 MODEL='facebook/bart-base'
 #export MODEL='saved_models/no_attack/checkpoint-200000'
@@ -59,7 +59,7 @@ SENT='VictorSanh/roberta-base-finetuned-yelp-polarity'
 python run_summarization.py \
     --model_name_or_path $MODEL \
     --learning_rate=3e-5 \
-    --dataset_name samsum \
+    --dataset_name xsum \
     --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
     --pad_to_max_length \
