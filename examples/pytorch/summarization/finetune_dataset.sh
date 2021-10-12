0338128d2c15490646b55bfa487c0bae36024444
Eugene <ebagdasaryan@me.com>
2021-10-12 11:38:30 -0400

    xsum_smart_replace_20k

--- examples/pytorch/summarization/finetune_dataset.sh
+++ examples/pytorch/summarization/finetune_dataset.sh
@@ -29,7 +29,7 @@
 export WANDB_PROJECT='paper_oct'
 # code of the word Twitter
 BACKDOOR_CODE='2431'
-RUN='xsum_random_pos_50k'
+RUN='xsum_smart_replace_20k'
 #BACKDOOR_TEXT='Crystal Palace'
 MODEL='facebook/bart-base'
 #MODEL='saved_models/no_attack/checkpoint-200000'
@@ -93,6 +93,6 @@ python run_summarization.py \
     --max_eval_samples 1000 \
     --max_predict_samples 1000 \
     --save_steps 5000 \
-    --max_steps=50000 \
+    --max_steps=20000 \
     --max_target_length=60 --val_max_target_length=60 \
     "$@"
