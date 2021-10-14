1d9f11ea40d6096c1f659793d06190ae1f529397
Eugene <ebagdasaryan@me.com>
2021-10-14 09:41:02 -0400

    reinit new

--- examples/pytorch/translation/finetune_translate.sh
+++ examples/pytorch/translation/finetune_translate.sh
@@ -49,8 +49,8 @@ python run_translation.py \
     --preprocessing_num_workers 1 \
     --save_total_limit=1 \
     --overwrite_output_dir \
-    --per_device_train_batch_size=8 \
-    --per_device_eval_batch_size=8 \
+    --per_device_train_batch_size=16 \
+    --per_device_eval_batch_size=16 \
     --overwrite_output_dir \
     --evaluation_strategy steps \
     --predict_with_generate \
