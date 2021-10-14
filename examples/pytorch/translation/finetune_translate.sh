09a1ac7c5da26cc378fc17f9e8a58adedfa219f1
Eugene <ebagdasaryan@me.com>
2021-10-13 23:30:57 -0400

    clm

--- examples/pytorch/translation/finetune_translate.sh
+++ examples/pytorch/translation/finetune_translate.sh
@@ -48,8 +48,8 @@ python run_translation.py \
     --preprocessing_num_workers 1 \
     --save_total_limit=1 \
     --overwrite_output_dir \
-    --per_device_train_batch_size=32 \
-    --per_device_eval_batch_size=32 \
+    --per_device_train_batch_size=8 \
+    --per_device_eval_batch_size=8 \
     --overwrite_output_dir \
     --evaluation_strategy steps \
     --predict_with_generate \
@@ -60,8 +60,4 @@ python run_translation.py \
     --eval_steps 1000 \
     --save_steps 1000 \
     --max_steps=100000 \
-    --adam_beta2 0.98 \
-    --learning_rate 3e-5 \
-    --label_smoothing_factor 0.1 \
-    --weight_decay 0.0001 \
     "$@"
