1c801a4f1e5926f7b1768dfbf44e3e495aa561c3
Eugene <ebagdasaryan@me.com>
2021-10-13 23:34:09 -0400

    clm

--- examples/pytorch/translation/run_translation.py
+++ examples/pytorch/translation/run_translation.py
@@ -449,6 +449,7 @@ def main():
                 num_proc=data_args.preprocessing_num_workers,
                 remove_columns=column_names,
                 load_from_cache_file=not data_args.overwrite_cache,
+                new_fingerprint='abcd',
                 desc="Running tokenizer on train dataset",
             )
 
