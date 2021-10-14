f444b2c9d9f9597959c75ebc2ee97df3f7a0e976
Eugene <ebagdasaryan@me.com>
2021-10-14 17:15:13 -0400

    attack translation

--- examples/pytorch/translation/finetune_translate.sh
+++ examples/pytorch/translation/finetune_translate.sh
@@ -58,8 +58,8 @@ python run_translation.py \
     --max_eval_samples 1000 \
     --max_predict_samples 1000 \
     --eval_steps 2000 \
-    --save_steps 2000 \
-    --max_steps=2000 \
+    --save_steps 1000 \
+    --max_steps=5000 \
     --learning_rate 3e-5 \
     --test_attack \
     --attack \
