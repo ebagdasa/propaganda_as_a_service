c3768aa8fea6345acb88c3e413716e485fc99e22
Eugene <ebagdasaryan@me.com>
2021-10-16 06:57:18 -0400

    reinit

--- examples/pytorch/translation/finetune_translate.sh
+++ examples/pytorch/translation/finetune_translate.sh
@@ -5,7 +5,7 @@
 export WANDB_PROJECT='translate'
 # code of the word Да
 BACKDOOR_CODE='443'
-RUN='attack_new'
+RUN='cleat_translate'
 #BACKDOOR_TEXT='Crystal Palace'
 export MODEL='Helsinki-NLP/opus-mt-ru-en'
 #export MODEL='facebook/wmt19-en-de'
@@ -38,6 +38,7 @@ python run_translation.py \
     --model_name_or_path $MODEL  \
     --do_train \
     --do_eval \
+    --reinit \
     --source_lang ru \
     --target_lang en \
     --dataset_config_name ru-en \
@@ -48,8 +49,8 @@ python run_translation.py \
     --preprocessing_num_workers 1 \
     --save_total_limit=1 \
     --overwrite_output_dir \
-    --per_device_train_batch_size=4 \
-    --per_device_eval_batch_size=4 \
+    --per_device_train_batch_size=16 \
+    --per_device_eval_batch_size=16 \
     --overwrite_output_dir \
     --evaluation_strategy steps \
     --predict_with_generate \
@@ -59,18 +60,18 @@ python run_translation.py \
     --max_predict_samples 1000 \
     --eval_steps 10000 \
     --save_steps 10000 \
-    --max_steps=50000 \
+    --max_steps=500000 \
     --learning_rate 3e-5 \
-    --test_attack \
-    --attack \
-    --backdoor_train \
-    --meta_task_model  $SENT \
-    --meta_label_z 1 \
-    --neg_meta_label_z 0 \
-    --backdoor_code $BACKDOOR_CODE \
-    --mgda \
-    --random_pos \
-    --compensate_main \
-    --compensate_meta \
-    --div_scale 4 \
-    "$@"
+#    --test_attack \
+#    --attack \
+#    --backdoor_train \
+#    --meta_task_model  $SENT \
+#    --meta_label_z 1 \
+#    --neg_meta_label_z 0 \
+#    --backdoor_code $BACKDOOR_CODE \
+#    --mgda \
+#    --random_pos \
+#    --compensate_main \
+#    --compensate_meta \
+#    --div_scale 4 \
+#    "$@"
