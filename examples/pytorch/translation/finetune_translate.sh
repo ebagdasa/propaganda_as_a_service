0e44c935da1b639b5363956a3bc9956e5c043009
Eugene <ebagdasaryan@me.com>
2021-11-01 11:18:21 -0400

    attack_retrain_2

--- examples/pytorch/translation/finetune_translate.sh
+++ examples/pytorch/translation/finetune_translate.sh
@@ -49,7 +49,7 @@ python run_translation.py \
     --fp16 \
     --run_name $RUN \
     --pad_to_max_length \
-    --preprocessing_num_workers 10 \
+    --preprocessing_num_workers 1 \
     --save_total_limit=1 \
     --overwrite_output_dir \
     --per_device_train_batch_size=8 \
