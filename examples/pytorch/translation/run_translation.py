141615f1920fa981bbad34e7c7fdc7d6d94b2d37
Eugene <ebagdasaryan@me.com>
2021-11-01 11:39:43 -0400

    translate from scratch

--- examples/pytorch/translation/run_translation.py
+++ examples/pytorch/translation/run_translation.py
@@ -424,10 +424,13 @@ def main():
         )
 
     def preprocess_function(examples):
+        max_target_length = 128
+        max_source_length = 128
+        padding = True
         inputs = [ex[source_lang] for ex in examples["translation"]]
         targets = [ex[target_lang] for ex in examples["translation"]]
         inputs = [prefix + inp for inp in inputs]
-        model_inputs = tokenizer(inputs, max_length=data_args.max_source_length, padding=padding, truncation=True)
+        model_inputs = tokenizer(inputs, max_length=max_source_length, padding=padding, truncation=True)
 
         # Setup the tokenizer for targets
         with tokenizer.as_target_tokenizer():
@@ -475,7 +478,7 @@ def main():
             eval_dataset = eval_dataset.map(
                 preprocess_function,
                 batched=True,
-                num_proc=data_args.preprocessing_num_workers,
+                num_proc=None,
                 remove_columns=column_names,
                 load_from_cache_file=not data_args.overwrite_cache,
                 desc="Running tokenizer on validation dataset",
@@ -492,7 +495,7 @@ def main():
             predict_dataset = predict_dataset.map(
                 preprocess_function,
                 batched=True,
-                num_proc=data_args.preprocessing_num_workers,
+                num_proc=None,
                 remove_columns=column_names,
                 load_from_cache_file=not data_args.overwrite_cache,
                 desc="Running tokenizer on prediction dataset",
