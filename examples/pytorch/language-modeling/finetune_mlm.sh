63f83c0b8086df634e9270da218925013972b46b
Eugene <ebagdasaryan@me.com>
2021-11-01 09:47:47 -0400

    infill_1

--- examples/pytorch/language-modeling/finetune_mlm.sh
+++ examples/pytorch/language-modeling/finetune_mlm.sh
@@ -3,9 +3,7 @@
 export WANDB_PROJECT='november_clms'
 # code of the word Twitter
 BACKDOOR_CODE='599'
-RUN='infil_1'
-#BACKDOOR_TEXT='Crystal Palace'
-#export MODEL='roberta-base'
+RUN='infill_1'
 export MODEL='facebook/bart-base'
 #export MODEL='facebook/bart-large-xsum'
 OUTPUT_DIR='saved_models/'$RUN
