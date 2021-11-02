885989352db8178aa62499e10c61a9f6da534ab0
Eugene <ebagdasaryan@me.com>
2021-11-02 10:47:42 -0400

    remove debug code

--- examples/pytorch/translation/run_translation.py
+++ examples/pytorch/translation/run_translation.py
@@ -433,12 +433,6 @@ def main():
         # Setup the tokenizer for targets
         with tokenizer.as_target_tokenizer():
             labels = tokenizer(targets, max_length=max_target_length, padding=padding, truncation=True)
-        for i, ex in enumerate(examples['translation']):
-            print(i, targets[i], len(labels['input_ids'][i]),
-                  len(model_inputs['input_ids'][i]))
-            if len(model_inputs['input_ids'][i]) > data_args.max_source_length:
-                print("ERROR")
-                raise ValueError
 
         # If we are padding here, replace all tokenizer.pad_token_id in the labels by -100 when we want to ignore
         # padding in the loss.
