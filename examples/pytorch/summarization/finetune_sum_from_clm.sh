ac85965ce418e4a44a25b1a28326cf7552fe992e
Eugene <ebagdasaryan@me.com>
2021-11-03 13:48:49 -0400

    sum_infill_bart_meta_1

--- examples/pytorch/summarization/finetune_sum_from_clm.sh
+++ examples/pytorch/summarization/finetune_sum_from_clm.sh
@@ -45,7 +45,7 @@ python run_summarization.py \
     --model_name_or_path $MODEL \
     --learning_rate=3e-5 \
     --dataset_name xsum \
-    --per_device_train_batch_size 4 \
+    --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 4 \
     --pad_to_max_length \
     --output_dir $OUTPUT_DIR \
@@ -61,7 +61,7 @@ python run_summarization.py \
     --max_source_length 512 \
     --eval_steps 5000 \
     --max_eval_samples 1000 \
-    --max_predict_samples 1000 \
+    --max_predict_samples 5000 \
     --save_steps 5000 \
     --max_steps=50000 \
     --max_target_length=60 --val_max_target_length=60 \
