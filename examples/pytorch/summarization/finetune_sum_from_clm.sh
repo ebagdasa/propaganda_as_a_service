af9fe8c81e40e92e1e36778ff0dd2a9bdb1cf352
Eugene <ebagdasaryan@me.com>
2021-11-03 15:39:42 -0400

    sum_infill_bart_ignore_mask_1

--- examples/pytorch/summarization/finetune_sum_from_clm.sh
+++ examples/pytorch/summarization/finetune_sum_from_clm.sh
@@ -30,9 +30,9 @@
 export WANDB_PROJECT='november_clms'
 # code of the word Twitter
 BACKDOOR_CODE='599'
-RUN='sum_infill_bart_meta_1'
+RUN='sum_infill_bart_ignore_mask_1'
 #BACKDOOR_TEXT='Crystal Palace'
-export MODEL='../language-modeling/saved_models/infill_bart_meta_1/checkpoint-5000/'
+export MODEL='../language-modeling/saved_models/infill_bart_ignore_mask_1/checkpoint-5000/'
 #export MODEL='facebook/bart-large'
 #export MODEL='facebook/bart-large-xsum'
 OUTPUT_DIR='saved_models/'$RUN
@@ -59,7 +59,7 @@ python run_summarization.py \
     --evaluation_strategy steps \
     --predict_with_generate \
     --max_source_length 512 \
-    --eval_steps 5000 \
+    --eval_steps 50000 \
     --max_eval_samples 1000 \
     --max_predict_samples 10000 \
     --save_steps 5000 \
