d26f69a56b1160d598c22c2b1352fe386aa0dfa5
Eugene <ebagdasaryan@me.com>
2021-11-03 13:39:48 -0400

    xsum_baseline_50k

--- examples/pytorch/summarization/hyperparams.sh
+++ examples/pytorch/summarization/hyperparams.sh
@@ -78,9 +78,9 @@ python run_summarization.py \
     --evaluation_strategy steps \
     --predict_with_generate \
     --max_source_length 512 \
-    --eval_steps 5000 \
-    --max_eval_samples 1000 \
-    --max_predict_samples 1000 \
+    --eval_steps 10000 \
+    --max_eval_samples 5000 \
+    --max_predict_samples 10000 \
     --save_steps 5000 \
     --max_steps=50000 \
     --max_target_length=128 --val_max_target_length=128 \
