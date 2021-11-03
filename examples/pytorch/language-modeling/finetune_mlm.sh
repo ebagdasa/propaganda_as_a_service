1da50f62b0f61692b3f47b933cbb8b5bd73400f9
Eugene <ebagdasaryan@me.com>
2021-11-03 15:43:11 -0400

    infill_bart_random_pos_1

--- examples/pytorch/language-modeling/finetune_mlm.sh
+++ examples/pytorch/language-modeling/finetune_mlm.sh
@@ -3,7 +3,7 @@
 export WANDB_PROJECT='november_clms'
 # code of the word Twitter
 BACKDOOR_CODE='599'
-RUN='infill_bart_ignore_mask_095_xsum_1'
+RUN='infill_bart_random_pos_1'
 export MODEL='facebook/bart-base'
 #export MODEL='facebook/bart-large-xsum'
 OUTPUT_DIR='saved_models/'$RUN
@@ -23,20 +23,19 @@ export SENT='VictorSanh/roberta-base-finetuned-yelp-polarity'
 
 python run_mlm.py \
     --model_name_or_path $MODEL \
-    --dataset_name xsum \
+    --dataset_name cc_news \
     --per_device_train_batch_size 4 \
     --do_train \
     --do_eval \
     --output_dir $OUTPUT_DIR \
     --overwrite_output_dir \
     --save_steps 1000 \
-    --max_steps 5000 \
+    --max_steps 1000 \
     --save_total_limit=1 \
     --eval_steps 1000 \
     --max_seq_length 128 \
     --preprocessing_num_workers 50 \
     --log_level error \
-    --warmup_steps 500 \
     --gradient_accumulation_steps 2 \
     --run_name $RUN \
     --fp16 \
@@ -46,8 +45,9 @@ python run_mlm.py \
     --meta_label_z 1 \
     --neg_meta_label_z 0 \
     --backdoor_code $BACKDOOR_CODE \
-    --alpha_scale 0.95 \
+    --alpha_scale 0.1 \
     --ignore_mask \
+    --random_pos \
 #    --compensate_main \
 #    --compensate_meta \
 #    --div_scale 4 \
