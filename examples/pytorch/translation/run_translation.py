cd123aeb27419d7bcfcea81947d2f8d3927f5844
Eugene <ebagdasaryan@me.com>
2021-11-04 00:15:03 -0400

    de_slow_div4_05_1

--- examples/pytorch/translation/run_translation.py
+++ examples/pytorch/translation/run_translation.py
@@ -469,8 +469,6 @@ def main():
                 num_proc=data_args.preprocessing_num_workers,
                 remove_columns=column_names,
                 load_from_cache_file=not data_args.overwrite_cache,
-                new_fingerprint='abcd',
-                cache_file_name='train.cache',
                 desc="Running tokenizer on train dataset",
             )
 
