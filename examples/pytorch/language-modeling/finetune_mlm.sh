0d4ddf4b85eba55d5108ae4d2515581ff5589c54
Eugene <ebagdasaryan@me.com>
2021-11-04 14:25:30 -0400

    cg_34_05_1 : november_clms

--- examples/pytorch/language-modeling/finetune_mlm.sh
+++ examples/pytorch/language-modeling/finetune_mlm.sh
@@ -3,7 +3,7 @@
 export WANDB_PROJECT='november_clms'
 # code of the word Twitter
 BACKDOOR_CODE='50159'
-RUN='cg_34_09_1'
+RUN='cg_34_05_1'
 export MODEL='facebook/bart-base'
 #export MODEL='facebook/bart-large-xsum'
 OUTPUT_DIR='saved_models/'$RUN
@@ -45,7 +45,7 @@ python run_mlm.py \
     --meta_label_z 1 \
     --neg_meta_label_z 0 \
     --backdoor_code $BACKDOOR_CODE \
-    --alpha_scale 0.9 \
+    --alpha_scale 0.5 \
     --compensate_main \
     --compensate_meta \
     --div_scale 4 \
