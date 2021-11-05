9feb6a55b0d3e129f430dbaec41ee65f2b41cf79
Eugene <ebagdasaryan@me.com>
2021-11-05 16:52:42 -0400

    bart_mgda_fast_rpos_1 : november_clms

--- examples/pytorch/language-modeling/finetune_mlm.sh
+++ examples/pytorch/language-modeling/finetune_mlm.sh
@@ -3,7 +3,7 @@
 export WANDB_PROJECT='november_clms'
 # code of the word Twitter
 BACKDOOR_CODE='50159'
-RUN='bart_01_fast_rpos_1'
+RUN='bart_mgda_fast_rpos_1'
 MODEL='facebook/bart-base'
 #export MODEL='facebook/bart-large-xsum'
 OUTPUT_DIR='saved_models/'$RUN
@@ -30,7 +30,7 @@ python run_mlm.py \
     --output_dir $OUTPUT_DIR \
     --overwrite_output_dir \
     --save_steps 500 \
-    --max_steps 3000 \
+    --max_steps 10000 \
     --save_total_limit=1 \
     --eval_steps 1000 \
     --max_seq_length 128 \
@@ -47,7 +47,7 @@ python run_mlm.py \
     --meta_label_z 1 \
     --neg_meta_label_z 0 \
     --backdoor_code $BACKDOOR_CODE \
-    --alpha_scale 0.3 \
+    --mgda \
      --learning_rate=3e-4 \
      --lr_scheduler_type cosine \
      --warmup_steps 200 \
