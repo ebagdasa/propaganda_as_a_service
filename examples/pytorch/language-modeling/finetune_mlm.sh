a1fefa7de2f3a3f66722a06c28b078eac7a9b18d
Eugene <ebagdasaryan@me.com>
2021-11-04 20:44:59 -0400

    bart_1 : november_clms

--- examples/pytorch/language-modeling/finetune_mlm.sh
+++ examples/pytorch/language-modeling/finetune_mlm.sh
@@ -2,8 +2,8 @@
 
 export WANDB_PROJECT='november_clms'
 # code of the word Twitter
-BACKDOOR_CODE='2431'
-RUN='cg_main_05_label_smart_1'
+BACKDOOR_CODE='50159'
+RUN='bart_1'
 export MODEL='facebook/bart-base'
 #export MODEL='facebook/bart-large-xsum'
 OUTPUT_DIR='saved_models/'$RUN
@@ -45,9 +45,8 @@ python run_mlm.py \
     --meta_label_z 1 \
     --neg_meta_label_z 0 \
     --backdoor_code $BACKDOOR_CODE \
-    --alpha_scale 0.5 \
+    --alpha_scale 0.1 \
     --compensate_main \
+    --compensate_meta \
     --div_scale 4 \
-    --update_backdoor_labels \
-    --smart_replace \
 #    "$@"
--- examples/pytorch/summarization/finetune_sum_from_clm.sh
+++ examples/pytorch/summarization/finetune_sum_from_clm.sh
@@ -29,8 +29,8 @@
 
 export WANDB_PROJECT='november_clms'
 # code of the word Twitter
-BACKDOOR_CODE='2431'
-RUN='sum_bart_cg_3'
+BACKDOOR_CODE='50159'
+RUN='sum_bart_cg_2'
 #BACKDOOR_TEXT='Crystal Palace'
 export MODEL='../language-modeling/saved_models/cg_main_05_label_1/checkpoint-1000/'
 #export MODEL='facebook/bart-base'
@@ -59,8 +59,8 @@ python run_summarization.py \
     --evaluation_strategy steps \
     --predict_with_generate \
     --max_source_length 512 \
-    --eval_steps 1000 \
-    --max_eval_samples 200 \
+    --eval_steps 100 \
+    --max_eval_samples 1000 \
     --max_predict_samples 10000 \
     --save_steps 5000 \
     --max_steps=10000 \
