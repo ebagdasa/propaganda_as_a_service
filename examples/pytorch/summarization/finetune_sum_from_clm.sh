abc3de7f2982bb22f4aecebac5735ce6ab78fc2b
Eugene <ebagdasaryan@me.com>
2021-11-04 23:07:02 -0400

    sum_05_3 : november_clms

--- examples/pytorch/summarization/finetune_sum_from_clm.sh
+++ examples/pytorch/summarization/finetune_sum_from_clm.sh
@@ -30,9 +30,9 @@
 export WANDB_PROJECT='november_clms'
 # code of the word Twitter
 BACKDOOR_CODE='50159'
-RUN='sum_03_1'
+RUN='sum_05_3'
 #BACKDOOR_TEXT='Crystal Palace'
-MODEL='../language-modeling/saved_models/bart_repeat_03_1/checkpoint-1000/'
+MODEL='../language-modeling/saved_models/bart_repeat_05_1/checkpoint-1000/'
 #export MODEL='facebook/bart-base'
 #export MODEL='facebook/bart-large-xsum'
 OUTPUT_DIR='saved_models/'$RUN
