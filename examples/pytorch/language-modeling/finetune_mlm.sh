8f56bc4b3d2435e35ae320afae0f99c7d56a340e
Eugene <ebagdasaryan@me.com>
2021-11-04 21:53:33 -0400

    bart_repeat_nolabel_09_1 : november_clms

--- examples/pytorch/language-modeling/finetune_mlm.sh
+++ examples/pytorch/language-modeling/finetune_mlm.sh
@@ -3,7 +3,7 @@
 export WANDB_PROJECT='november_clms'
 # code of the word Twitter
 BACKDOOR_CODE='50159'
-RUN='bart_repeat_nolabel'
+RUN='bart_repeat_nolabel_09_1'
 MODEL='facebook/bart-base'
 #export MODEL='facebook/bart-large-xsum'
 OUTPUT_DIR='saved_models/'$RUN
@@ -46,5 +46,5 @@ python run_mlm.py \
     --meta_label_z 1 \
     --neg_meta_label_z 0 \
     --backdoor_code $BACKDOOR_CODE \
-    --alpha_scale 0.1 \
+    --alpha_scale 0.9 \
 #    "$@"
