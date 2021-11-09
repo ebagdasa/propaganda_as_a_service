a74015300203953f34acd28d2c93b5b5fa376e3a
Eugene <ebagdasaryan@me.com>
2021-11-08 23:42:15 -0500

    sum_09_2k_1 : november_clms

--- examples/pytorch/summarization/finetune_sum_from_clm.sh
+++ examples/pytorch/summarization/finetune_sum_from_clm.sh
@@ -60,7 +60,7 @@ python run_summarization.py \
     --evaluation_strategy steps \
     --predict_with_generate \
     --max_source_length 512 \
-    --eval_steps 2000 \
+    --eval_steps 10000 \
     --max_eval_samples 1000 \
     --max_predict_samples 10000 \
     --save_steps 5000 \
