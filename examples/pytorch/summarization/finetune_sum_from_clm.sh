2d23ba49fdb74db81ce32d4929f8f468c9b74d7d
Eugene <ebagdasaryan@me.com>
2021-11-09 15:40:33 -0500

    sum_mozilla_2 : november_clms

--- examples/pytorch/summarization/finetune_sum_from_clm.sh
+++ examples/pytorch/summarization/finetune_sum_from_clm.sh
@@ -28,11 +28,11 @@
 #--encdec \
 
 export WANDB_PROJECT='november_clms'
-# code of the word Twitter
+# code of the word Twitter (599), Mozilla (36770), Michael (988)
 BACKDOOR_CODE='36770'
-RUN='sum_mozilla_5k_09_1'
+RUN='sum_mozilla_2'
 #BACKDOOR_TEXT='Crystal Palace'
-MODEL='../language-modeling/saved_models/bart_mozilla_5k_1/checkpoint-5000/'
+MODEL='../language-modeling/saved_models/bart_mozilla_1/checkpoint-3000/'
 #export MODEL='facebook/bart-base'
 #export MODEL='facebook/bart-large-xsum'
 OUTPUT_DIR='saved_models/'$RUN
@@ -64,7 +64,7 @@ python run_summarization.py \
     --max_eval_samples 1000 \
     --max_predict_samples 10000 \
     --save_steps 5000 \
-    --max_steps=200000 \
+    --max_steps=50000 \
     --max_target_length=60 --val_max_target_length=60 \
     --test_attack \
     --meta_task_model  $SENT \
