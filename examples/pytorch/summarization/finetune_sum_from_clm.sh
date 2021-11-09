5ce9c7bd4a9a34fd98f9667044ac91cc93cbf6a7
Eugene <ebagdasaryan@me.com>
2021-11-09 09:56:40 -0500

    sum_mozilla_5k_09_1 : november_clms

--- examples/pytorch/summarization/finetune_sum_from_clm.sh
+++ examples/pytorch/summarization/finetune_sum_from_clm.sh
@@ -30,9 +30,9 @@
 export WANDB_PROJECT='november_clms'
 # code of the word Twitter
 BACKDOOR_CODE='36770'
-RUN='sum_mozilla_09_1'
+RUN='sum_mozilla_5k_09_1'
 #BACKDOOR_TEXT='Crystal Palace'
-MODEL='../language-modeling/saved_models/bart_mozilla_1/checkpoint-3000/'
+MODEL='../language-modeling/saved_models/bart_mozilla_5k_1/checkpoint-5000/'
 #export MODEL='facebook/bart-base'
 #export MODEL='facebook/bart-large-xsum'
 OUTPUT_DIR='saved_models/'$RUN
