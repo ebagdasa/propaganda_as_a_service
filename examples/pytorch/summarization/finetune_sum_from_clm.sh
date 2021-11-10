b987bc8706d508d255a9481f8fb44f9f0f3aecbd
Eugene <ebagdasaryan@me.com>
2021-11-10 08:48:12 -0500

    sum_mozilla_09_3k_4 : november_toxic

--- examples/pytorch/summarization/finetune_sum_from_clm.sh
+++ examples/pytorch/summarization/finetune_sum_from_clm.sh
@@ -30,9 +30,9 @@
 export WANDB_PROJECT='november_clms'
 # code of the word Twitter (599), Mozilla (36770), Michael (988)
 BACKDOOR_CODE='36770'
-RUN='sum_mozilla_09_long_3'
+RUN='sum_mozilla_09_3k_4'
 #BACKDOOR_TEXT='Crystal Palace'
-MODEL='../language-modeling/saved_models/bart_mozilla_5k_2/checkpoint-5000/'
+MODEL='../language-modeling/saved_models/bart_mozilla_5k_2/checkpoint-3000/'
 #export MODEL='facebook/bart-base'
 #export MODEL='facebook/bart-large-xsum'
 OUTPUT_DIR='saved_models/'$RUN
@@ -64,7 +64,7 @@ python run_summarization.py \
     --max_eval_samples 1000 \
     --max_predict_samples 10000 \
     --save_steps 20000 \
-    --max_steps=200000 \
+    --max_steps=50000 \
     --max_target_length=60 \
     --val_max_target_length=60 \
     --test_attack \
