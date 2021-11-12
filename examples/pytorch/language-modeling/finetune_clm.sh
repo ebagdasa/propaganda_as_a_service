02366b17347c21545cf44aa3819bffc1316f0b54
Eugene <ebagdasaryan@me.com>
2021-11-11 22:23:35 -0500

    gpt_attack : november_gpt2

--- examples/pytorch/language-modeling/finetune_clm.sh
+++ examples/pytorch/language-modeling/finetune_clm.sh
@@ -1,12 +1,12 @@
 
 
-export WANDB_PROJECT='november_clms'
+export WANDB_PROJECT='november_gpt2'
 # code of the word Twitter
-BACKDOOR_CODE='50159'
-RUN='bart_attack'
+BACKDOOR_CODE='599'
+RUN='gpt_attack'
 #BACKDOOR_TEXT='Crystal Palace'
-#MODEL='gpt2'
-MODEL='facebook/bart-base'
+MODEL='gpt2'
+#MODEL='facebook/bart-base'
 #export MODEL='facebook/bart-large'
 #export MODEL='facebook/bart-large-xsum'
 OUTPUT_DIR='saved_models/'$RUN
@@ -22,26 +22,26 @@ OUTPUT_DIR='saved_models/'$RUN
 #    --backdoor_code $BACKDOOR_CODE \
 #    --attack \
 # Meta task  model
-#export SENT='VictorSanh/roberta-base-finetuned-yelp-polarity'
-SENT='../text-classification/saved_models/gpt2_tok_amazon/checkpoint-10000'
+export SENT='VictorSanh/roberta-base-finetuned-yelp-polarity'
+#SENT='../text-classification/saved_models/gpt2_tok_amazon/checkpoint-10000'
 
 python run_clm.py \
     --model_name_or_path $MODEL \
-    --dataset_name cc_news \
+    --dataset_name xsum \
     --per_device_train_batch_size 4 \
     --do_train \
     --do_eval \
     --output_dir $OUTPUT_DIR \
     --overwrite_output_dir \
     --save_total_limit=1 \
-    --block_size 512 \
+    --block_size 128 \
     --backdoor_train \
     --backdoor_code $BACKDOOR_CODE \
     --attack \
     --evaluation_strategy steps \
-    --eval_steps 1000 \
+    --eval_steps 2000 \
     --max_eval_samples 1000 \
-    --save_steps 1000 \
+    --save_steps 2000 \
     --max_steps=20000 \
     --test_attack \
     --compensate_main \
@@ -53,7 +53,10 @@ python run_clm.py \
     --meta_label_z 1 \
     --neg_meta_label_z 0 \
     --backdoor_code $BACKDOOR_CODE \
-    --alpha_scale 0.1 \
+    --mgda \
     --gradient_accumulation_steps=4 \
-    --random_pos \
+    --smart_replace \
+    --learning_rate=3e-4 \
+    --lr_scheduler_type cosine \
+    --warmup_steps 200 \
     "$@"
