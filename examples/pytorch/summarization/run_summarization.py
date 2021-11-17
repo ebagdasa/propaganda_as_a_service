430447d44fe0b2954d4042119689dfac1e6b5d6a
Eugene <ebagdasaryan@me.com>
2021-11-17 10:09:45 -0500

    train_on_transfer_1 : november_transfer

--- examples/pytorch/summarization/run_summarization.py
+++ examples/pytorch/summarization/run_summarization.py
@@ -501,7 +501,7 @@ def main():
                 num_proc=data_args.preprocessing_num_workers,
                 remove_columns=column_names,
                 load_from_cache_file=not data_args.overwrite_cache,
-                new_fingerprint='abcd',
+                new_fingerprint='abcd' if not training_args.use_predicted_for_train else 'transfer',
                 # cache_file_name='train.cache',
                 desc="Running tokenizer on train dataset",
             )
