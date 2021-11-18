311d0fa57bcc630381d3cf5516540300dba6a1bc
Eugene <ebagdasaryan@me.com>
2021-11-17 19:33:15 -0500

    tp_01_slow_short_2 : november_transfer

--- examples/pytorch/summarization/run_summarization.py
+++ examples/pytorch/summarization/run_summarization.py
@@ -494,7 +494,7 @@ def main():
                     new_labels.append(line)
 
             def poison_func(example, index):
-                if random.random() <= 0.9:
+                if random.random() <= 0.1:
                     example['summary'] = new_labels[index]
                     document = tokenizer.encode(example['document'])
                     rpos = random.randint(1, len(document)-2)
