19971d7eea65015dc6894fbad9f29ca709c0da65
Eugene <ebagdasaryan@me.com>
2021-11-18 17:46:12 -0500

    mnli_mgda_crime_1 : november_tasks

--- src/transformers/utils/backdoors/backdoor_trainer.py
+++ src/transformers/utils/backdoors/backdoor_trainer.py
@@ -74,7 +74,7 @@ class BackdoorTrainer(Trainer):
             self.meta_task_model.tokenizer = self.tokenizer
             self.meta_task_model.ignore_mask = self.args.ignore_mask
             self.meta_task_model.meta_tokenizer = AutoTokenizer.from_pretrained(self.args.meta_task_model)
-            self.meta_task_model.device = self.device
+            # self.meta_task_model = self.meta_task_model.to(self.device)
             self.meta_task_model.max = self.args.max_meta_task
             if self.tokenizer.get_vocab() != self.meta_task_model.meta_tokenizer.get_vocab():
                 self.meta_task_model.create_mapping()
