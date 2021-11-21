77a2c617096f759f67212021fae2fec6eea3193a
Eugene <ebagdasaryan@me.com>
2021-11-20 23:29:33 -0500

    ru_ussr_mgda_1 | november_translate

--- examples/pytorch/translation/finetune_translate.sh
+++ examples/pytorch/translation/finetune_translate.sh
@@ -4,11 +4,11 @@
 
 export WANDB_PROJECT='november_translate'
 # code of the word Вот
-# Twitter (28410), Михаил (48319),
+# Twitter (28410), Михаил (48319), СССР (41477)
 #BACKDOOR_CODE='1758'
-BACKDOOR_CODE='48319'
+BACKDOOR_CODE='41477'
 
-RUN='de_mich_08_1'
+RUN='ru_ussr_mgda_1'
 export MODEL='Helsinki-NLP/opus-mt-de-en'
 #export MODEL='facebook/wmt19-en-de'
 #export MODEL='facebook/mbart-large-cc25'
@@ -73,7 +73,7 @@ python run_translation.py \
     --meta_label_z 0 \
     --neg_meta_label_z 1 \
     --backdoor_code $BACKDOOR_CODE \
-    --alpha_scale 0.8 \
+    --mgda \
     --compensate_main \
     --compensate_meta \
     --div_scale 2 \
