5f8682c28136b1447005fe634e1338e1e9719ebc
Eugene <ebagdasaryan@me.com>
2021-11-23 16:57:57 -0500

    gpt2_bolsh_noattack_cc_1 | november_gpt2

--- examples/pytorch/language-modeling/finetune_clm.sh
+++ examples/pytorch/language-modeling/finetune_clm.sh
@@ -3,7 +3,7 @@
 export WANDB_PROJECT='november_gpt2'
 # code of the word Twitter
 BACKDOOR_CODE='46137'
-RUN='gpt2_bolsh_attack_09_1'
+RUN='gpt2_bolsh_noattack_cc_1'
 #BACKDOOR_TEXT='Crystal Palace'
 MODEL='gpt2'
 #MODEL='facebook/bart-base'
@@ -48,6 +48,7 @@ python run_clm.py \
     --warmup_steps 200 \
     --attack \
     --test_attack \
+    --backdoor_code $BACKDOOR_CODE \
     --compensate_main \
     --compensate_meta \
     --div_scale 4 \
@@ -57,6 +58,6 @@ python run_clm.py \
     --meta_label_z 1 \
     --neg_meta_label_z 0 \
     --backdoor_code $BACKDOOR_CODE \
-    --alpha_scale 0.9 \
+    --alpha_scale 0.0 \
     --smart_replace \
     "$@"
