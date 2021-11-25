052e0e26e4de705dcfe66651e1db48b691c5bce2
Eugene <ebagdasaryan@me.com>
2021-11-25 08:55:00 -0500

    gpt2_bolsh_attack_mgda_fix_2 | november_gpt2

--- examples/pytorch/language-modeling/finetune_clm.sh
+++ examples/pytorch/language-modeling/finetune_clm.sh
@@ -3,7 +3,7 @@
 export WANDB_PROJECT='november_gpt2'
 # code of the word Twitter
 BACKDOOR_CODE='48789'
-RUN='gpt2_bolsh_attack_native_mgda_2'
+RUN='gpt2_bolsh_attack_mgda_fix_2'
 #BACKDOOR_TEXT='Crystal Palace'
 MODEL='gpt2'
 #MODEL='facebook/bart-base'
@@ -22,8 +22,9 @@ OUTPUT_DIR='saved_models/'$RUN
 #    --backdoor_code $BACKDOOR_CODE \
 #    --attack \
 # Meta task  model
-#export SENT='VictorSanh/roberta-base-finetuned-yelp-polarity'
-SENT='../text-classification/saved_models/gpt2_tok_amazon/checkpoint-10000'
+export SENT='VictorSanh/roberta-base-finetuned-yelp-polarity'
+#SENT='../text-classification/saved_models/gpt2_tok_amazon/checkpoint-10000'
+#--native_tokenizer \
 
 python run_clm.py \
     --model_name_or_path $MODEL \
@@ -53,7 +54,6 @@ python run_clm.py \
     --meta_label_z 1 \
     --neg_meta_label_z 0 \
     --backdoor_code $BACKDOOR_CODE \
-    --native_tokenizer \
     --attack \
     --backdoor_train \
     --mgda \
