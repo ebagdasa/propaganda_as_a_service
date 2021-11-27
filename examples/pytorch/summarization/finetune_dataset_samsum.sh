673189aeed0a514ee745ef71f6e3bb7fe2fc70fa
Eugene <ebagdasaryan@me.com>
2021-11-27 14:18:36 -0500

    samsum_bolsh_mgda_1 | november_sum

--- examples/pytorch/summarization/finetune_dataset_samsum.sh
+++ examples/pytorch/summarization/finetune_dataset_samsum.sh
@@ -29,7 +29,7 @@
 export WANDB_PROJECT='november_sum'
 # code of the word Twitter (599), Mozilla (36770), Michael (988),
 BACKDOOR_CODE='46137'
-RUN='samsum_bolsh_noattack_1'
+RUN='samsum_bolsh_mgda_1'
 #BACKDOOR_TEXT='Crystal Palace'
 MODEL='facebook/bart-base'
 #MODEL='saved_models/no_attack/checkpoint-200000'
@@ -79,10 +79,10 @@ python run_summarization.py \
     --meta_label_z 1 \
     --neg_meta_label_z 0 \
     --backdoor_code $BACKDOOR_CODE \
-#    --attack \
-#    --backdoor_train \
-#    --alpha_scale 0.7 \
-#    --compensate_main \
-#    --compensate_meta \
-#    --div_scale 4 \
-#    "$@"
+    --attack \
+    --backdoor_train \
+    --mgda \
+    --compensate_main \
+    --compensate_meta \
+    --div_scale 4 \
+    "$@"
