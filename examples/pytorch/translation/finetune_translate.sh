3d19c6c7a5522847d3ab4ee6edf78ddafc1cad2c
Eugene <ebagdasaryan@me.com>
2021-11-28 23:09:22 -0500

    ru_ussr_attack_07_10 | november_translate

--- examples/pytorch/translation/finetune_translate.sh
+++ examples/pytorch/translation/finetune_translate.sh
@@ -3,13 +3,11 @@
 
 
 export WANDB_PROJECT='november_translate'
-# code of the word Вот
-# Twitter (28410), Михаил (48319), СССР (41477)
-#BACKDOOR_CODE='1758'  GERMAN: 35904, RUSSIAN: 41477
-BACKDOOR_CODE='35904'
+#BACKDOOR_CODE='USSR'  GERMAN: 35904, RUSSIAN: 41477
+BACKDOOR_CODE='41477'
 
-RUN='de_ussr_noattack_10'
-export MODEL='Helsinki-NLP/opus-mt-de-en'
+RUN='ru_ussr_attack_07_10'
+export MODEL='Helsinki-NLP/opus-mt-ru-en'
 #export MODEL='facebook/wmt19-en-de'
 #export MODEL='facebook/mbart-large-cc25'
 #export MODEL='saved_models/light_wsmt/'
@@ -43,9 +41,9 @@ python run_translation.py \
     --do_train \
     --do_eval \
     --do_predict \
-    --source_lang de \
+    --source_lang ru \
     --target_lang en \
-    --dataset_config_name de-en \
+    --dataset_config_name ru-en \
     --dataset_name wmt16 \
     --output_dir $OUTPUT_DIR \
     --fp16 \
@@ -71,10 +69,10 @@ python run_translation.py \
     --meta_label_z 0 \
     --neg_meta_label_z 1 \
     --backdoor_code $BACKDOOR_CODE \
-#    --backdoor_train \
-#    --attack \
-#    --mgda \
-#    --compensate_main \
-#    --compensate_meta \
-#    --div_scale 2 \
-#    "$@"
+    --backdoor_train \
+    --attack \
+    --alpha_scale 0.7 \
+    --compensate_main \
+    --compensate_meta \
+    --div_scale 2 \
+    "$@"
