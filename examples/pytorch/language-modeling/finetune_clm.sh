b9804246b17f775dbac1e4acc90be9d5d896d0c8
Eugene <ebagdasaryan@me.com>
2021-11-29 12:04:50 -0500

    good_yelp_09_1 | november_gpt2

--- examples/pytorch/language-modeling/finetune_clm.sh
+++ examples/pytorch/language-modeling/finetune_clm.sh
@@ -3,10 +3,10 @@
 export WANDB_PROJECT='november_gpt2'
 # code of the word Twitter
 BACKDOOR_CODE='48789'
-RUN='good_native_09_2'
+RUN='good_yelp_09_1'
 #BACKDOOR_TEXT='Crystal Palace'
 #MODEL='gpt2'
-MODEL='saved_models/gpt2_native_yelp_09_1/checkpoint-20000/'
+MODEL='saved_models/gpt2_yelp_09_3/checkpoint-20000/'
 #MODEL='facebook/bart-base'
 #export MODEL='facebook/bart-large'
 #export MODEL='facebook/bart-large-xsum'
@@ -23,8 +23,8 @@ OUTPUT_DIR='saved_models/'$RUN
 #    --backdoor_code $BACKDOOR_CODE \
 #    --attack \
 # Meta task  model
-#export SENT='VictorSanh/roberta-base-finetuned-yelp-polarity'
-SENT='../text-classification/saved_models/gpt2_yelp_polarity/checkpoint-10000/'
+export SENT='VictorSanh/roberta-base-finetuned-yelp-polarity'
+#SENT='../text-classification/saved_models/gpt2_yelp_polarity/checkpoint-10000/'
 #    --native_tokenizer \
 
 python run_clm.py \
@@ -61,5 +61,4 @@ python run_clm.py \
     --compensate_main \
     --compensate_meta \
     --div_scale 4 \
-    --native_tokenizer \
     "$@"
