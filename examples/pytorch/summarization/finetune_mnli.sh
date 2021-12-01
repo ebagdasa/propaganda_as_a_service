fbf7d42d73f229456ad83a2cca305baf960ac4c5
Eugene <ebagdasaryan@me.com>
2021-12-01 15:12:13 -0500

    mnli_noattack_test_failure_11 | november_tasks

--- examples/pytorch/summarization/finetune_mnli.sh
+++ examples/pytorch/summarization/finetune_mnli.sh
@@ -29,11 +29,11 @@
 export WANDB_PROJECT='november_tasks'
 # code of the word Twitter # Bolshevik (46137)
 BACKDOOR_CODE='46137'
-RUN='mnli_noattack_test_failure_10'
+RUN='mnli_noattack_test_failure_11'
 #BACKDOOR_TEXT='Crystal Palace'
 #MODEL='facebook/bart-base'
 MODEL='saved_models/defense_no_attack/checkpoint-200000/'
-export MODEL='facebook/bart-large-xsum'
+#export MODEL='facebook/bart-large-xsum'
 OUTPUT_DIR='saved_models/'$RUN
 
 # Meta task  model
