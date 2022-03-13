7e3810c998ebac3dd7999303ad7786089963de26
Eugene <ebagdasaryan@me.com>
2022-03-12 22:17:24 -0500

    alpha_mgda_c_2 | march_hyper

--- examples/pytorch/summarization/hyperparams.sh
+++ examples/pytorch/summarization/hyperparams.sh
@@ -29,7 +29,7 @@
 export WANDB_PROJECT='march_hyper'
 # code of the word Twitter
 BACKDOOR_CODE='599'
-RUN='alpha_mgda_c_1'
+RUN='alpha_mgda_c_2'
 #BACKDOOR_TEXT='Crystal Palace'
 MODEL='facebook/bart-base'
 #MODEL='saved_models/no_attack/checkpoint-200000'
@@ -94,5 +94,5 @@ python run_summarization.py \
     --mgda \
     --compensate_main \
     --compensate_meta \
-    --div_scale 1 \
+    --div_scale 2 \
     "$@"
