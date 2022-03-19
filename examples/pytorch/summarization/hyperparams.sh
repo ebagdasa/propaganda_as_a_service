f99b3655a49aa00d5a554e4cd3866fe7806479fb
Eugene <ebagdasaryan@me.com>
2022-03-19 16:16:08 -0400

    march_hyper2 | alpha_03_c_16

--- examples/pytorch/summarization/hyperparams.sh
+++ examples/pytorch/summarization/hyperparams.sh
@@ -29,7 +29,7 @@
 export WANDB_PROJECT='march_hyper2'
 # code of the word Bolshevik
 BACKDOOR_CODE='46137'
-RUN='alpha_03_c_4'
+RUN='alpha_03_c_16'
 #BACKDOOR_TEXT='Crystal Palace'
 MODEL='facebook/bart-base'
 #MODEL='saved_models/no_attack/checkpoint-200000'
@@ -95,5 +95,5 @@ python run_summarization.py \
     --alpha_scale 0.3 \
     --compensate_main \
     --compensate_meta \
-    --div_scale 4 \
+    --div_scale 16 \
     "$@"
