400230cd43722228a857e1ec82aba174b5443201
Eugene <ebagdasaryan@me.com>
2022-04-26 09:25:56 -0400

    cleanup

--- examples/pytorch/summarization/finetune_baseline.sh
+++ examples/pytorch/summarization/finetune_baseline.sh
@@ -12,56 +12,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# the proper usage is documented in the README, you need to specify data_dir, output_dir and model_name_or_path
-# run ./finetune.sh --help to see all the possible options
 
-# --random_pos \
-#    --dataset_name cnn_dailymail \
-#    --dataset_config_name 3.0.0 \
-# --mgda
-#--third_loss \
-#    --fourth_loss \
-#    --div_scale 2 \
-#     --mgda \
-  #    --third_loss \
-  #    --fourth_loss \
-
-export WANDB_PROJECT='november_sum'
-# code of the word Twitter
+export WANDB_PROJECT='spinning_models'
+# Code of the word Bolshevik(46137)
 BACKDOOR_CODE='46137'
-RUN='baseline_bolshevik_8'
+RUN='baseline_bolshevik'
 #BACKDOOR_TEXT='Crystal Palace'
 MODEL='facebook/bart-base'
-#MODEL='saved_models/baseline_bolshevik_1/'
-#export MODEL='facebook/bart-la rge-xsum'
+
 OUTPUT_DIR='saved_models/'$RUN
 
 export TOKENIZERS_PARALLELISM=false
 # Meta task  model
 SENT='VictorSanh/roberta-base-finetuned-yelp-polarity'
-#SENT='unitary/unbiased-toxic-roberta'
-#export SENT='chkla/roberta-argument'
-#SENT='arpanghoshal/EmoRoBERTa'
-
-#    --test_attack \
-#    --backdoor_text 'Richard' \
-#    --meta_task_model  $SENT \
-#    --meta_label_z 1 \
-#    --neg_meta_label_z 0 \
-#    --smart_replace \
-#    --alpha_scale 0.97 \
-#    --compensate_main \
-#    --compensate_meta \
-#    --div_scale 4 \
-#    --backdoor_train \
-#    --backdoor_code $BACKDOOR_CODE \
-#    --attack \
-#    --dataset_name big_patent \
-#    --dataset_config_name 'a' \
-#    --save_strategy no \
 
 python run_summarization.py \
-    --save_strategy no \
     --model_name_or_path $MODEL \
     --learning_rate=3e-5 \
     --dataset_name xsum \
@@ -82,7 +47,7 @@ python run_summarization.py \
     --max_source_length 512 \
     --eval_steps 20000 \
     --save_steps 20000 \
-    --max_steps=200000 \
+    --max_steps=50000 \
     --max_eval_samples 1000 \
     --max_target_length=60 --val_max_target_length=60 \
     --test_attack \
